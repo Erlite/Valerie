efa0696fd23dd54fc47a16b8261b2b74a8b7c17b
ExceptionDev <Yivlx@outlook.com>
2017-03-19 11:12:19 -0700

    Gif and memes Icon too. Forget to compile... :hammer:

--- GPB/Modules/GeneralModule.cs
+++ GPB/Modules/GeneralModule.cs
@@ -1,31 +1,121 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord.Commands;
 using Discord;
-using Discord.WebSocket;
-using GPB.Services;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace GPB.Modules
 {
     public class GeneralModule : ModuleBase
     {
-        [Command("Test")]
-        public async Task TestAsync()
+        [Command("GuildInfo"), Summary("GI"), Remarks("Displays information about a guild")]
+        public async Task GuildInfo()
         {
-            var embed = new EmbedBuilder()
-        .WithTitle("===== Kicked User =====")
-        .WithDescription($"**kjhajkshdjkahsjd**")
-        .WithAuthor(x =>
+            var embed = new EmbedBuilder();
+            var gld = Context.Guild;
+            if (!string.IsNullOrWhiteSpace(gld.IconUrl))
+                embed.ThumbnailUrl = gld.IconUrl;
+            var I = gld.Id;
+            var O = gld.GetOwnerAsync().GetAwaiter().GetResult().Mention;
+            var D = gld.GetDefaultChannelAsync().GetAwaiter().GetResult().Mention;
+            var V = gld.VoiceRegionId;
+            var C = gld.CreatedAt;
+            var A = gld.Available;
+            var N = gld.DefaultMessageNotifications;
+            var E = gld.IsEmbeddable;
+            var L = gld.MfaLevel;
+            var R = gld.Roles;
+            var VL = gld.VerificationLevel;
+            embed.Color = new Color(153, 30, 87);
+            embed.Title = $"{gld.Name} Information";
+            embed.Description = $"**Guild ID: **{I}\n**Guild Owner: **{O}\n**Default Channel: **{D}\n**Voice Region: **{V}\n**Created At: **{C}\n**Available? **{A}\n" +
+                $"**Default Msg Notif: **{N}\n**Embeddable? **{E}\n**MFA Level: **{L}\n**Verification Level: **{VL}\n";
+            await ReplyAsync("", false, embed);
+        }
+
+        [Command("Gif"), Summary("Gif Cute kittens"), Remarks("Searches gif for your Gifs??")]
+        public async Task gifs([Remainder] string keywords)
         {
-            x.Name = Context.User.Username;
-            x.IconUrl = Context.User.GetAvatarUrl();
-        })
-        .WithColor(new Color(232, 226, 53))
-        .WithFooter(x =>
+            if (string.IsNullOrWhiteSpace(keywords))
+                throw new ArgumentException("What do you want me to search for?");
+
+            var getUrl = new Uri("http://api.giphy.com/");
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = getUrl;
+                var response = await client.GetAsync(Uri.EscapeDataString($"v1/gifs/random?api_key=dc6zaTOxFJmzC&tag={Uri.UnescapeDataString(keywords)}"));
+                response.EnsureSuccessStatusCode();
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                var obj = JObject.Parse(jsonResponse);
+
+                var embed = new EmbedBuilder();
+                embed.Author = new EmbedAuthorBuilder()
+                {
+                    Name = $"{Context.User.Username} searched for {keywords}",
+                    IconUrl = Context.User.GetAvatarUrl()
+                };
+                embed.ImageUrl = obj["data"]["image_original_url"].ToString();
+                embed.Color = new Color(153, 30, 87);
+
+                await ReplyAsync("", false, embed);
+            }
+        }
+
+        [Command("Urban")]
+        public async Task Urban([Remainder] string urban = null)
         {
-            x.Text = $"Kicked by {Context.User}";
-            x.IconUrl = Context.User.GetAvatarUrl();
-        });
+            if (string.IsNullOrWhiteSpace(urban))
+                throw new NullReferenceException("Please provide a search term");
+            var embed = new EmbedBuilder();
+            var vc = new HttpClient();
+            embed.WithAuthor(x =>
+            {
+                x.Name = "Urban Dictionary";
+                x.WithIconUrl("https://lh3.googleusercontent.com/4hpSJ4pAfwRUg-RElZ2QXNh_pV01Z96iJGT2BFuk_RRsNc-AVY7cZhbN2g1zWII9PBQ=w170");
+            });
+            string req = await vc.GetStringAsync("http://api.urbandictionary.com/v0/define?term=" + urban);
+            embed.WithColor(new Color(153, 30, 87));
+
+            MatchCollection col = Regex.Matches(req, @"(?<=definition"":"")[ -z~-🧀]+(?="",""permalink)");
+            MatchCollection col2 = Regex.Matches(req, @"(?<=example"":"")[ -z~-🧀]+(?="",""thumbs_down)");
+            if (col.Count == 0)
+            {
+                await ReplyAsync("Couldn't find anything dammit");
+                return;
+            }
+            Random r = new Random();
+            string outpt = "Failed fetching embed from Urban Dictionary, please try later!";
+            string outpt2 = "No Example";
+            int max = r.Next(0, col.Count);
+            for (int i = 0; i <= max; i++)
+            {
+                outpt = urban + "\r\n\r\n" + col[i].Value;
+            }
+
+            for (int i = 0; i <= max; i++)
+            {
+                outpt2 = "\r\n\r\n" + col2[i].Value;
+            }
+
+            outpt = outpt.Replace("\\r", "\r");
+            outpt = outpt.Replace("\\n", "\n");
+            outpt2 = outpt2.Replace("\\r", "\r");
+            outpt2 = outpt2.Replace("\\n", "\n");
+
+            embed.AddField(x =>
+            {
+                x.Name = $"Definition";
+                x.Value = outpt;
+            });
+
+            embed.AddField(x =>
+            {
+                x.Name = "Example";
+                x.Value = outpt2;
+            });
+
             await ReplyAsync("", embed: embed);
         }
     }
