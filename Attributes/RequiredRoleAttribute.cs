c63958a7f797a0457ac0e47d4d333307de254164
ExceptionDev <Yivlx@outlook.com>
2017-04-17 18:21:06 -0700

    Required Role Attribute :bell:

---
+++ Attributes/RequiredRoleAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rick.Attributes
+{
+    public class RequiredRoleAttribute : PreconditionAttribute
+    {
+        public override async Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            var user = context.User as IGuildUser;
+            return await Task.FromResult(user) ? PreconditionResult.FromSuccess() : PreconditionResult.FromError($"{Format.Bold("ERROR: ")}Role is missing! Please get the appropriate role for this command!");
+        }
+    }
+}
