1b18ae522d98f649b13da34fb8de3959297361d7
ExceptionDev <Yivlx@outlook.com>
2017-04-21 14:22:26 -0700

    Added Interfaces for configs

---
+++ Interfaces/IBotConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rick.Interfaces
+{
+    public interface IBotConfig
+    {
+        string BotToken { get; set; }
+        string DefaultPrefix { get; set; }
+        string BingAPIKey { get; set; }
+        bool DebugMode { get; set; }
+        bool ClientLatency { get; set; }
+    }
+}
---
+++ Interfaces/IGuildConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rick.Interfaces
+{
+    public interface IGuildConfig
+    {
+        string GuildPrefix { get; set; }
+        ulong ModChannelID { get; set; }
+        bool JoinLogs { get; set; }
+        bool LeaveLogs { get; set; }
+        bool NameChangesLogged { get; set; }
+        bool NickChangesLogged { get; set; }
+        bool UserBannedLogged { get; set; }
+        bool MessageRecieve { get; set; }
+        ulong[] RequiredRoleID { get; set;}
+        ulong[] RequiredChannelIDs { get; set; }
+        string[] RequiredChannelNames { get; set; }
+        Dictionary<string, string> Tags { get; set; }
+    }
+}
