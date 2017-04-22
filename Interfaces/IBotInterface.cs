1817a598a871e28044813b35fec06b11ce280788
ExceptionDev <Yivlx@outlook.com>
2017-04-22 14:37:15 -0700

    Created Bot interface

---
+++ Interfaces/IBotInterface.cs
@@ -0,0 +1,12 @@
+﻿namespace Rick.Interfaces
+{
+    public interface IBotInterface
+    {
+        string BotToken { get; set; }
+        string DefaultPrefix { get; set; }
+        string BingAPIKey { get; set; }
+        bool MentionDefaultPrefix { get; set; }
+        bool DebugMode { get; set; }
+        bool ClientLatency { get; set; }
+    }
+}
