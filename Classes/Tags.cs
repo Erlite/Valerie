1ac0fda0dde11f60c389018b77c9590226946c31
ExceptionDev <Yivlx@outlook.com>
2017-04-28 13:21:57 -0700

    Added more values

--- Classes/Tags.cs
+++ Classes/Tags.cs
@@ -4,7 +4,8 @@
     {
         public string TagName { get; set; }
         public string TagResponse { get; set; }
-        public ulong OwnerId { get; set; }
         public string CreationDate { get; set; }
+        public ulong OwnerId { get; set; }
+        public int TagUses { get; set; }
     }
 }
