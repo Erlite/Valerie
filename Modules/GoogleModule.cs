008afb6a7621aa2b2e8d4a50f8a24e83cd058e7e
ExceptionDev <Yivlx@outlook.com>
2017-05-06 15:59:56 -0700

    added google search command

--- Modules/GoogleModule.cs
+++ Modules/GoogleModule.cs
@@ -1,18 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
-using Discord;
 using Discord.Commands;
 using Rick.Services;
 using Rick.Classes;
+using System.Linq;
 using Rick.Attributes;
+using Rick.Handlers;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Services;
 
 namespace Rick.Modules
 {
     [Group("Google"), CheckBlacklist]
     public class GoogleModule : ModuleBase
     {
+        [Command("Search"), Summary("Google Seach Very wow"), Remarks("Seaches google for your terms")]
+        public async Task SearchAsync([Remainder] string search)
+        {
+            var Str = new StringBuilder();
+            var Service = new CustomsearchService(new BaseClientService.Initializer
+            {
+                ApiKey = BotHandler.BotConfig.GoogleAPIKey
+            });
+            var RequestList = Service.Cse.List(search);
+            RequestList.Cx = BotHandler.BotConfig.SearchEngineID;
+
+            var items = RequestList.Execute().Items.Take(5);
+            foreach (var result in items)
+            {
+                Str.AppendLine($"**=> {result.Title}**\n{result.Link}");
+            }
+            string URL = "http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png";
+            var embed = EmbedService.Embed(EmbedColors.Pastle, $"Search for: {search}", Context.Client.CurrentUser.GetAvatarUrl(), null, Str.ToString(), null, null, null, URL);
+            await ReplyAsync("", embed: embed);
+        }
+
+        [Command("Image"), Summary("Google Image doges"), Remarks("Searches google for your image")]
+        public async Task ImageAsync([Remainder] string search)
+        {
+
+        }
     }
 }
