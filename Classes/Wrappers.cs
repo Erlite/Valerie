e826e823e4e92215aaf3875ab05e51cddf7bc7f5
ExceptionDev <Yivlx@outlook.com>
2017-05-10 19:54:52 -0700

    Eh fudge it. No wrappers

--- Classes/Wrappers.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rick.Classes
-{
-    public class EventWrapper
-    {
-        public bool IsEnabled { get; private set; }
-        public string TextChannel { get; private set; }
-    }
-}
