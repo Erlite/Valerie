5c4439d337cbf34565ae84bafb2253e3cfc3204b
ExceptionDev <Yivlx@outlook.com>
2017-05-20 11:14:53 -0700

    Added steam news command

---
+++ Classes/SteamClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rick.Classes
+{
+    public class Newsitem
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string contents { get; set; }
+        public string feedlabel { get; set; }
+    }
+
+    public class Appnews
+    {
+        public List<Newsitem> newsitems { get; set; }
+        public int count { get; set; }
+    }
+
+    public class SteamAppNews
+    {
+        public Appnews appnews { get; set; }
+    }
+}
---
+++ Modules/SteamModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rick.Attributes;
+using Rick.Classes;
+using Discord;
+using Discord.Commands;
+using Rick.Services;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Rick.Modules
+{
+    [CheckBlacklist, Group("Steam")]
+    public class SteamModule : ModuleBase
+    {
+        [Command("News")]
+        public async Task NewsAsync(int ID)
+        {
+            var Httpclient = new HttpClient();
+            var RequestUrl = await Httpclient.GetAsync($"http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid={ID}&count=5&maxlength=300&format=json");
+            if (!RequestUrl.IsSuccessStatusCode)
+            {
+                await ReplyAsync(RequestUrl.ReasonPhrase);
+                return;
+            }
+            var Content = await RequestUrl.Content.ReadAsStringAsync();
+            var Convert = JToken.Parse(Content).ToObject<SteamAppNews>();
+
+            var Builder = new EmbedBuilder()
+            {
+                Author = new EmbedAuthorBuilder()
+                {
+                    Name = $"APP ID: {ID}",
+                    IconUrl = Context.User.GetAvatarUrl()
+                },
+                Footer = new EmbedFooterBuilder()
+                {
+                    Text = $"Total Results: {Convert.appnews.count.ToString()}"
+                },
+                Color = new Color(124,12,57)
+            };
+
+            foreach(var Result in Convert.appnews.newsitems)
+            {
+                Builder.AddField(x =>
+                {
+                    x.Name = $"{Result.title} || {Result.feedlabel}";
+                    x.Value = $"{Result.contents}\n{Result.url}";
+                });
+            }
+            await ReplyAsync("", embed: Builder);
+        }
+    }
+}
