9839f0efafcced08e83a593fdd50ee5d51e046fc
ExceptionDev <Yivlx@outlook.com>
2017-05-25 14:07:51 -0700

    Separate class and module for giphy

---
+++ Classes/GiphyClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Rick.Classes
+{
+    public class Datum
+    {
+        public string embed_url { get; set; }
+    }
+
+    public class GiphyRoot
+    {
+        public List<Datum> data { get; set; }
+    }
+}
---
+++ Modules/GiphyModule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Rick.Classes;
+using Rick.Attributes;
+using Rick.Services;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Rick.Handlers;
+using Newtonsoft.Json;
+
+namespace Rick.Modules
+{
+    [CheckBlacklist, Group("Giphy"), ]
+    public class GiphyModule : ModuleBase
+    {
+        string GifsEndpoint = "https://api.giphy.com/v1/gifs/";
+        string StickersEndpoint = "http://api.giphy.com/v1/stickers/";
+        string Key = $"api_key={BotHandler.BotConfig.GiphyAPIKey}";
+
+        [Command(""), Summary("Gif Cute kittens"), Remarks("Searches Giphy for your Gifs??"), Alias("Gif"), Priority(0)]
+        public async Task GiphyAsync([Remainder] string Terms)
+        {
+            var GetUrl = GifsEndpoint + $"search?q={Terms.Replace(' ', '+')}&" + Key;
+            var Response = await new HttpClient().GetAsync(GetUrl);
+            if (!Response.IsSuccessStatusCode)
+            {
+                await ReplyAsync(Response.ReasonPhrase);
+                return;
+            }
+            var ConvertedJson = JsonConvert.DeserializeObject<GiphyRoot>(await Response.Content.ReadAsStringAsync());
+            var items = new Random().Next(0, ConvertedJson.data.Count);
+            await ReplyAsync(ConvertedJson.data[items].embed_url);
+        }
+
+        [Command("Tag"), Summary("Giphy Tag Kittens"), Remarks("Searches Giphy for your tag"), Priority(1)]
+        public async Task TagsAsync(string Tag)
+        {
+            var GetUrl = GifsEndpoint + "random?" + Key + $"&tag={Tag}";
+            var Response = await new HttpClient().GetAsync(GetUrl);
+            if (!Response.IsSuccessStatusCode)
+            {
+                await ReplyAsync(Response.ReasonPhrase);
+                return;
+            }
+            var ConvertedJson = JsonConvert.DeserializeObject<GiphyRoot>(await Response.Content.ReadAsStringAsync());
+            var items = new Random().Next(0, ConvertedJson.data.Count);
+            await ReplyAsync(ConvertedJson.data[items].embed_url);
+
+        }
+
+        [Command("Sitkcers"), Summary("Giphy Stickers Dank Memes"), Remarks("Animated stickers rather than gifs")]
+        public async Task StickersAsync([Remainder] string Search)
+        {
+            var GetUrl = StickersEndpoint + $"search?q={Search.Replace(' ', '+')}&" + Key;
+            var Response = await new HttpClient().GetAsync(GetUrl);
+            if (!Response.IsSuccessStatusCode)
+            {
+                await ReplyAsync(Response.ReasonPhrase);
+                return;
+            }
+            var ConvertedJson = JsonConvert.DeserializeObject<GiphyRoot>(await Response.Content.ReadAsStringAsync());
+            var items = new Random().Next(0, ConvertedJson.data.Count);
+            await ReplyAsync(ConvertedJson.data[items].embed_url);
+        }
+    }
+}
