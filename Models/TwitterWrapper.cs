b52daf95ebbbab8a089c6331ca967712c4865e08
ExceptionDev <Yivlx@outlook.com>
2017-06-10 12:58:43 -0700

    Created wrappers

---
+++ Models/TwitterWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Rick.Models
+{
+    public class TwitterWrapper
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenSecret { get; set; }
+    }
+}
