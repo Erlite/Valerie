2969963ac217e0429e0174c4492c17cb504bc4b0
ExceptionDev <Yivlx@outlook.com>
2017-06-18 10:38:12 -0700

    idek

--- Services/MsgsService.cs
+++ Services/MsgsService.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Threading;
 using Discord.WebSocket;
 using Rick.Handlers;
 using CleverbotLib.Models;
