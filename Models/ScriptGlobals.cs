9f616ecf446b540445042529b41894a9f884d01e
ExceptionDev <Yivlx@outlook.com>
2017-06-21 10:22:15 -0700

    Fixed eval globals

--- Models/ScriptGlobals.cs
+++ Models/ScriptGlobals.cs
@@ -5,14 +5,10 @@ namespace Rick.Models
 {
     public class ScriptGlobals
     {
-        public CommandContext Context { get; internal set; }
+        public ICommandContext Context { get; internal set; }
         public DiscordSocketClient Client { get; internal set; }
-        public SocketGuildChannel Channel => Context.Channel as SocketGuildChannel;
-        public SocketGuildUser User => Context.User as SocketGuildUser;
-        public SocketReaction Reaction => Context.Message.Reactions as SocketReaction;
-        public SocketGuild Guild => Context.Guild as SocketGuild;
-        public SocketMessage Message => Context.Message as SocketMessage;
-        public SocketUser SocketUser => Context.User as SocketUser;
-        public SocketRole Role => Context.Guild.Roles as SocketRole;
+        public SocketGuildUser SocketGuildUser { get; internal set; }
+        public SocketGuild SocketGuild { get; internal set; }
+        public SocketGuildChannel SocketGuildChannel { get; internal set; }
     }
 }
