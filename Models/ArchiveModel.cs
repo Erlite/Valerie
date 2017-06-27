a0b761e9ec0b64fbfae7a3535689f87f4d68b1cc
ExceptionDev <Yivlx@outlook.com>
2017-06-27 11:38:24 -0700

    Added archive model

---
+++ Models/ArchiveModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rick.Models
+{
+    public class ArchiveModel
+    {
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
