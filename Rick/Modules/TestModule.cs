11536a83185e0c10a9834b5c4311a43a4cb2bc62
ExceptionDev <Yivlx@outlook.com>
2017-06-29 08:46:33 -0700

    Ran test and success

--- Rick/Modules/TestModule.cs
+++ Rick/Modules/TestModule.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Rick.Functions;
 
 namespace Rick.Modules
 {
     public class TestModule : ModuleBase
     {
         [Command("Test")]
-        public async Task TestAsync()
+        public async Task TestAsync(string Msg)
         {
-            await ReplyAsync("Working");
+            string Filter = Function.Censor(Msg);
+            await ReplyAsync(Filter);
         }
     }
 }
