1756fc8b82d32be0f9e1021e737a3267eec0c43b
Yucked <Yivlx@outlook.com>
2017-07-26 12:30:52 -0700

    w/e

--- Rick/Modules/TestModule.cs
+++ Rick/Modules/TestModule.cs
@@ -1,40 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Discord.Commands;
-using Rick.Controllers;
-using Rick.Handlers;
-using Rick.Functions;
-using Discord.WebSocket;
-using System.Linq;
-using Discord.Audio;
-using Discord;
-using System.Threading;
-using System.Net.Http;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Discord.Commands;
+//using Rick.Controllers;
+//using Rick.Handlers;
+//using Rick.Functions;
+//using Discord.WebSocket;
+//using System.Linq;
+//using Discord.Audio;
+//using Discord;
+//using System.Threading;
+//using System.Net.Http;
+//using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json;
 
-namespace Rick.Modules
-{
-    //private readonly Timer _timer;
+//namespace Rick.Modules
+//{
+//    private readonly Timer _timer;
 
-    //public TestModule( DiscordSocketClient Client)
-    //{
-    //    _timer = new Timer(async _ =>
-    //    {
-    //        var chn = Client.GetChannel(232558894554152960) as IMessageChannel;
-    //        await chn.SendMessageAsync("test");
-    //    },
-    //    null,
-    //    TimeSpan.FromSeconds(20),
-    //    TimeSpan.FromSeconds(5));
-    //}
-    //[Command("test")]
-    //public async Task test()
-    //{
-    //}
-    public class TestModule : ModuleBase
-    {
-    }
-}
+//    public TestModule(DiscordSocketClient Client)
+//    {
+//        _timer = new Timer(async _ =>
+//        {
+//            var chn = Client.GetChannel(232558894554152960) as IMessageChannel;
+//            await chn.SendMessageAsync("test");
+//        },
+//        null,
+//        TimeSpan.FromSeconds(20),
+//        TimeSpan.FromSeconds(5));
+//    }
+//    [Command("test")]
+//    public async Task test()
+//    {
+//    }
+//    public class TestModule : ModuleBase
+//    {
+//        [Command("test")]
+//        public async Task Testasync([Remainder]string msg)
+//        {
+
+//        }
+//    }
+//}
