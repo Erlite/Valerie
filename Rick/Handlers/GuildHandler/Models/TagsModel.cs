1a8f80742fcb060322da2c65fe1fe8f80cc719b1
Yucked <Yivlx@outlook.com>
2017-07-30 07:44:35 -0700

    db bug

--- Rick/Handlers/GuildHandler/Models/TagsModel.cs
+++ Rick/Handlers/GuildHandler/Models/TagsModel.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public string Response { get; set; }
         public string CreationDate { get; set; }
-        public ulong Owner { get; set; }
+        public string Owner { get; set; }
         public int Uses { get; set; }
     }
 }
