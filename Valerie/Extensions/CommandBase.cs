068efd13eb90e77f5a79d47cd117819c7e9bdc0c
Yucked <Yivlx@outlook.com>
2017-08-05 15:24:14 -0700

    CommandBase > ModuleBase because TriggerTyping

---
+++ Valerie/Extensions/CommandBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Valerie.Extensions
+{
+    public class CommandBase : ModuleBase<ICommandContext>
+    {
+        protected override async Task<IUserMessage> ReplyAsync(string message, bool isTTS = false, Embed embed = null, RequestOptions options = null)
+        {
+            await Context.Channel.TriggerTypingAsync();
+            return await Context.Channel.SendMessageAsync(message, isTTS, embed, options);
+        }
+    }
+}
