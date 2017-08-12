a4fd32376f30351cd5d945c39a8aa2c020ed97c5
Yucked <Yivlx@outlook.com>
2017-08-12 12:33:21 -0700

    Updated NSFW Attribute

---
+++ Valerie/Attributes/RequireNSFW.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Valerie.Attributes
+{
+    public class RequireNSFW : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
+        {
+            if (IsNSFW(Context.Channel))
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            else
+                return Task.FromResult(PreconditionResult.FromError("Command can only be ran in NSFW channel, pervert."));
+        }
+
+        static bool IsNSFW(IChannel Channel) =>
+            Channel.Name == "nsfw" || Channel.Name.Contains("nsfw") || Channel.Name.StartsWith("nsfw-") ||
+            Channel.Name.StartsWith("nsfw_");
+    }
+}
