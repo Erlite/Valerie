f5e88e1efff5c405fa6a242dfe1c60b4ae560d69
Yivlx <vuxdev@gmail.com>
2017-08-13 09:01:34 -0700

    Urban Dictionary model

---
+++ Valerie/Models/Urban.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Valerie.Models
+{
+    public class List
+    {
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+        [JsonProperty("word")]
+        public string Word { get; set; }
+        [JsonProperty("example")]
+        public string Example { get; set; }
+    }
+
+    public class Urban
+    {
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+        [JsonProperty("list")]
+        public List<List> List { get; set; }
+    }
+}
