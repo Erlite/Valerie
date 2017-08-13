b55967c7e911288aea8c17a0aa01404f399fb43c
Yivlx <vuxdev@gmail.com>
2017-08-13 14:31:58 -0700

    Update karma giving

--- Valerie/Extensions/IntExtension.cs
+++ Valerie/Extensions/IntExtension.cs
@@ -4,21 +4,15 @@ namespace Valerie.Extensions
 {
     public class IntExtension
     {
-        public static int GiveKarma(int Karma)
+        public static int GiveKarma(int KarmaToGive, int TotalUsers)
         {
-            return (int)(Math.Pow(Karma, 2) + 25 * Karma ) / 10;
+            return (int)Math.Ceiling(Math.Pow(KarmaToGive, 2) / TotalUsers);
         }
 
         public static int GetLevel(int Karma)
         {
             return 1 + (int)Math.Floor(Math.Pow(Karma, 1 / 3.0));
         }
-
-        public static int GetKarmaForLastLevel(int Level)
-        {
-            return (Level - 1) * (Level - 1) * (Level - 1);
-        }
-
         public static int GetKarmaForNextLevel(int Level)
         {
             return Level * Level * Level;
