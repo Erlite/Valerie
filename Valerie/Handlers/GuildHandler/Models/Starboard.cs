2d81991c022676c29c6eda1ca2d3769bc7a2bea9
Yivlx <vuxdev@gmail.com>
2017-08-14 12:58:31 -0700

    Starboard model

---
+++ Valerie/Handlers/GuildHandler/Models/Starboard.cs
@@ -0,0 +1,10 @@
+﻿namespace Valerie.Handlers.GuildHandler.Models
+{
+    public class Starboard
+    {
+        public string MessageId { get; set; }
+        public string ChannelId { get; set; }
+        public string StarboardMessageId { get; set; }
+        public int Stars { get; set; }
+    }
+}
