35a84e49ed82c777681173b55df0b758403d65cd
Yivlx <vuxdev@gmail.com>
2017-08-18 08:10:52 -0700

    Change Karma formula

--- Valerie/Extensions/IntExtension.cs
+++ Valerie/Extensions/IntExtension.cs
@@ -4,9 +4,15 @@ namespace Valerie.Extensions
 {
     public class IntExtension
     {
-        public static int GiveKarma(int KarmaToGive, int TotalUsers) => (int)Math.Ceiling(Math.Pow(KarmaToGive, 3) / Math.Pow(TotalUsers, 2));
+        public static int GiveKarma(int KarmaToGive, int TotalUsers)
+        {
+            if (KarmaToGive < 500)
+                return KarmaToGive;
+            else
+                return KarmaToGive / (int)Math.Sqrt(TotalUsers);
+        }
 
-        public static int GetLevel(int Karma) => 1 + (int)Math.Floor(Math.Pow(Karma, 1 / 4.0));
+        public static int GetLevel(int Karma) => 1 + (int)Math.Pow(Karma, 1 / 4.0);
 
         public static int GetKarmaForNextLevel(int Level) => Level * Level * Level;
     }
