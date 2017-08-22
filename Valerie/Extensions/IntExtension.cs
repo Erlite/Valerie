7dfba15253f9357648b1ff59d30bae5bcedcbe79
Yivlx <vuxdev@gmail.com>
2017-08-22 16:13:11 -0700

    Schmeckles Baby

--- Valerie/Extensions/IntExtension.cs
+++ Valerie/Extensions/IntExtension.cs
@@ -15,5 +15,9 @@ namespace Valerie.Extensions
         public static int GetLevel(int Eridium) => 1 + (int)Math.Pow(Eridium, 1 / 4.0);
 
         public static int GetEridiumForNextLevel(int Level) => (int)Math.Pow(Level, 4);
+
+        public static int ConvertToSchmeckles(int EridiumToConvert) => (int)Math.Pow(EridiumToConvert, 1 / 5);
+
+        public static int ConvertToEridium(int SchmecklesToConvert) => (int)Math.Pow(SchmecklesToConvert, 5);
     }
 }
