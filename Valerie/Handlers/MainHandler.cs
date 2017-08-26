7be3859255a728f9c50d49ac975f1d0eef22ff20
Yucked <vuxdev@gmail.com>
2017-08-26 14:32:24 -0700

    Cleanup

--- Valerie/Handlers/MainHandler.cs
+++ Valerie/Handlers/MainHandler.cs
@@ -14,22 +14,14 @@ namespace Valerie.Handlers
         public static string CacheFolder = Path.Combine(Directory.GetCurrentDirectory(), "Cache");
         static Lazy<IDocumentStore> DocumentStore = new Lazy<IDocumentStore>(CreateDocStore);
 
-        static IDocumentStore CreateDocStore()
+        static IDocumentStore CreateDocStore => new DocumentStore()
         {
-            return new DocumentStore()
-            {
-                Database = "Valerie",
-                Urls = new string[] { "http://localhost:8080" }
-            }.Initialize();
-        }
+            Database = "Valerie",
+            Urls = new string[] { "http://localhost:8080" }
+        }.Initialize();
 
-        public static IDocumentStore Store
-        {
-            get
-            {
-                return DocumentStore.Value;
-            }
-        }
+
+        public static IDocumentStore Store => DocumentStore.Value;
 
         public static async Task GetReadyAsync()
         {
@@ -41,10 +33,10 @@ namespace Valerie.Handlers
             var AuthUser = User.GetAuthenticatedUser();
             if (AuthUser == null)
             {
-                Log.Write(Status.ERR, Source.BotDatabase, ExceptionHandler.GetLastException().TwitterDescription);
+                Log.Write(Status.ERR, Source.Config, ExceptionHandler.GetLastException().TwitterDescription);
             }
             else
-                Log.Write(Status.KAY, Source.BotDatabase, "Logged into Twitter.");
+                Log.Write(Status.KAY, Source.Config, "Logged into Twitter.");
             if (!Directory.Exists(CacheFolder))
                 Directory.CreateDirectory(CacheFolder);
         }
