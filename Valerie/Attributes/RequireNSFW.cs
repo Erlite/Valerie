5eb96a3e4e6d2c1f459658a821b6a03a3dd9f3cc
Yucked <vuxdev@gmail.com>
2017-08-28 21:14:17 -0700

    Updated NSFW Precondition

--- Valerie/Attributes/RequireNSFW.cs
+++ Valerie/Attributes/RequireNSFW.cs
@@ -9,7 +9,7 @@ namespace Valerie.Attributes
     {
         public override Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
         {
-            if (BoolExtension.IsNSFW(Context.Channel))
+            if (BoolExtension.IsNSFW(Context.Channel as Discord.ITextChannel))
                 return Task.FromResult(PreconditionResult.FromSuccess());
             else
                 return Task.FromResult(PreconditionResult.FromError("Command can only be ran in NSFW channel, pervert."));
