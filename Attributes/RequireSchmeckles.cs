5b0f9ac4ff77a4bffed369b471153dc97e9dc522
Yivlx <vuxdev@gmail.com>
2017-09-05 18:10:08 -0700

    Change int to double

--- Attributes/RequireSchmeckles.cs
+++ Attributes/RequireSchmeckles.cs
@@ -8,9 +8,9 @@ namespace Valerie.Attributes
 {
     public class RequireSchmeckles : PreconditionAttribute
     {
-        int Schmeckles { get; }
+        double Schmeckles { get; }
 
-        public RequireSchmeckles(int RequiredSchmeckles = 25)
+        public RequireSchmeckles(double RequiredSchmeckles = 25)
         {
             Schmeckles = RequiredSchmeckles;
         }
@@ -18,16 +18,17 @@ namespace Valerie.Attributes
         public override async Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
         {
             var GuildConfig = await ServerConfig.ConfigAsync(Context.Guild.Id).ConfigureAwait(false);
-
+            var Config = ServerConfig.Config;
             var GetUserEridium = GuildConfig.EridiumHandler.UsersList[Context.User.Id];
             var UserSchmeckles = IntExtension.ConvertToSchmeckles(GetUserEridium);
             int ConvertedEridium = IntExtension.ConvertToEridium(Schmeckles);
 
-           if (Schmeckles > UserSchmeckles)
+            if (Schmeckles > UserSchmeckles)
                 return await Task.FromResult(PreconditionResult.FromError($"{Discord.Format.Bold(Info.Name)} requires **{Schmeckles}** Schmeckles."));
             else
             {
-                //await ServerDB.EridiumHandlerAsync(Context.Guild.Id, ModelEnum.EridiumSubtract, Context.User.Id, ConvertedEridium);
+                Config.EridiumHandler.UsersList[Context.User.Id] -= ConvertedEridium;
+                await ServerConfig.SaveAsync().ConfigureAwait(false);
                 return await Task.FromResult(PreconditionResult.FromSuccess());
             }
         }
