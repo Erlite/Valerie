f5e5842ac381590c9c4a2174c557a32d24b7d507
Yivlx <vuxdev@gmail.com>
2017-09-07 17:31:48 -0700

    BBC

---
+++ Models/BBC.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Valerie.Models
+{
+    public class Article
+    {
+        [JsonProperty("author")]
+        public string Author { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("urlToImage")]
+        public string ImageUrl { get; set; }
+        [JsonProperty("publishedAt")]
+        public string PublishDate { get; set; }
+    }
+
+    public class BBC
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("source")]
+        public string Source { get; set; }
+        [JsonProperty("sortBy")]
+        public string SortBy { get; set; }
+        [JsonProperty("articles")]
+        public List<Article> Articles { get; set; }
+    }
+}
