e6d636870a71fab1e8c4f988b4bce6f6d6c275c4
Yivlx <vuxdev@gmail.com>
2017-09-19 20:57:55 -0700

    Refelect Config Changes

--- Attributes/RequireSchmeckles.cs
+++ Attributes/RequireSchmeckles.cs
@@ -1,35 +1,35 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Discord.Commands;
-using Valerie.Handlers.Server;
 using Valerie.Extensions;
+using Valerie.Handlers.Server;
 
 namespace Valerie.Attributes
 {
     public class RequireSchmeckles : PreconditionAttribute
     {
         int Schmeckles { get; }
-
         public RequireSchmeckles(int RequiredSchmeckles = 25)
         {
             Schmeckles = RequiredSchmeckles;
         }
 
-        public override async Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
         {
-            var GuildConfig = await ServerConfig.ConfigAsync(Context.Guild.Id).ConfigureAwait(false);
-            var Config = ServerConfig.Config;
-            var GetUserEridium = GuildConfig.EridiumHandler.UsersList[Context.User.Id];
+            var GetService = Provider.GetService<ServerConfig>();
+            var Config = GetService.LoadConfig(Context.Guild.Id);
+            var GetUserEridium = Config.EridiumHandler.UsersList[Context.User.Id];
             var UserSchmeckles = IntExtension.ConvertToSchmeckles(GetUserEridium);
             int ConvertedEridium = IntExtension.ConvertToEridium(Schmeckles);
 
             if (Schmeckles > UserSchmeckles)
-                return await Task.FromResult(PreconditionResult.FromError($"{Discord.Format.Bold(Info.Name)} requires **{Schmeckles}** Schmeckles."));
+                return Task.FromResult(PreconditionResult.FromError($"{Discord.Format.Bold(Info.Name)} requires **{Schmeckles}** Schmeckles."));
             else
             {
-                Config.EridiumHandler.UsersList[Context.User.Id] -= ConvertedEridium;
-                await ServerConfig.SaveAsync().ConfigureAwait(false);
-                return await Task.FromResult(PreconditionResult.FromSuccess());
+                Config.EridiumHandler.UsersList.TryUpdate(Context.User.Id, GetUserEridium - ConvertedEridium, GetUserEridium);
+                GetService.Save(Config, Context.Guild.Id);
+                return Task.FromResult(PreconditionResult.FromSuccess());
             }
         }
     }
