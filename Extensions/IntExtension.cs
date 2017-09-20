58d3416c55ed9c5554db64469181faa12a61a893
Yivlx <vuxdev@gmail.com>
2017-09-19 20:56:34 -0700

    Eridium Changes

--- Extensions/IntExtension.cs
+++ Extensions/IntExtension.cs
@@ -6,13 +6,13 @@ namespace Valerie.Extensions
     {
         public static int GiveEridium(int EridiumToGive, int TotalUsers)
         {
-            int GetTotal = (TotalUsers > 350) ? TotalUsers : (int)Math.Sqrt(TotalUsers);
-            return (EridiumToGive < 500) ? EridiumToGive : EridiumToGive / GetTotal;
+            int GetTotal = (TotalUsers > 450) ? TotalUsers : (int)Math.Sqrt(TotalUsers);
+            return (EridiumToGive < 50) ? EridiumToGive : EridiumToGive / GetTotal;
         }
 
-        public static int GetLevel(int Eridium) => 1 + (int)Math.Pow(Eridium, 1 / 4.0);
+        public static int GetLevel(int Eridium) => 1 + (int)Math.Pow(Eridium, 1 / 7.0);
 
-        public static int GetEridiumForNextLevel(int Level) => (int)Math.Pow(Level, 4);
+        public static int GetEridiumForNextLevel(int Level) => (int)Math.Pow(Level, 7);
 
         public static int ConvertToSchmeckles(int EridiumToConvert) => (int)Math.Pow(EridiumToConvert, 1 / 5);
 
