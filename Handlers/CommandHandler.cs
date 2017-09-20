83c0047ee5ab5d18d560b9d6b7c54435196bea85
Yivlx <vuxdev@gmail.com>
2017-09-19 20:46:29 -0700

    Init ValerieContext

--- Handlers/CommandHandler.cs
+++ Handlers/CommandHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,12 +6,11 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Valerie.Services;
-using Valerie.Handlers.Server;
 using Valerie.Handlers.Config;
 
 namespace Valerie.Handlers
 {
-    public class CommandHandler
+    class CommandHandler
     {
         IServiceProvider Provider;
         DiscordSocketClient Client;
@@ -27,52 +25,18 @@ namespace Valerie.Handlers
             CommandService = Provider.GetService<CommandService>();
         }
 
-        public async Task ConfigureCommandsAsync() =>
-            await CommandService.AddModulesAsync(Assembly.GetEntryAssembly());
+        public async Task ConfigureCommandsAsync() => await CommandService.AddModulesAsync(Assembly.GetEntryAssembly());
 
         async Task HandleMessagesAsync(SocketMessage Message)
         {
             var Msg = Message as SocketUserMessage;
-            var GuildConfig = await ServerConfig.ConfigAsync((Msg.Channel as SocketGuildChannel).Guild.Id).ConfigureAwait(false);
             int argPos = 0;
-
             if (Msg == null || Msg.Author.IsBot || BotConfig.Config.UsersBlacklist.ContainsKey(Msg.Author.Id)) return;
-            var Context = new SocketCommandContext(Client, Msg);
 
-            if (!(Msg.HasStringPrefix(BotConfig.Config.Prefix, ref argPos) || Msg.HasStringPrefix(GuildConfig.Prefix, ref argPos))) return;
+            var Context = new ValerieContext(Client, Msg as IUserMessage, Provider);
+            if (!(Msg.HasStringPrefix(BotConfig.Config.Prefix, ref argPos) || Msg.HasStringPrefix(Context.Config.Prefix, ref argPos))) return;
 
-            BotConfig.Config.CommandsUsed += 1;
-            await BotConfig.SaveAsync().ConfigureAwait(false);
-
-            var Result = await CommandService.ExecuteAsync(Context, argPos, Provider, MultiMatchHandling.Best);            
-            var Service = CommandService.Search(Context, argPos);
-
-            CommandInfo Command = null;
-            string ErrorMsg = null;
-
-            if (Result.IsSuccess)
-                return;
-
-            if (Service.IsSuccess)
-                Command = Service.Commands.FirstOrDefault().Command;
-
-            switch (Result)
-            {
-                case SearchResult SR: break;
-                case ExecuteResult ER:
-                    Log.Write(Log.Status.ERR, Log.Source.Client, ER.ErrorReason + "\n" + ER.Exception.StackTrace);
-                    break;
-                case PreconditionResult PdR:
-                    ErrorMsg = PdR.ErrorReason;
-                    Log.Write(Log.Status.ERR, Log.Source.Client, $"[{Context.Guild.Name} | {Context.User} | {Command.Name}]");
-                    break;
-                case ParseResult PR:
-                    ErrorMsg = $"{Format.Bold("Usage:")} {BotConfig.Config.Prefix}{Command.Name} {string.Join(" ", Command.Parameters.Select(x => $"`<{x.Name}>`"))}\n" +
-                        $"{Format.Bold("Summary:")} {Command.Summary}";
-                    break;
-            }
-
-            await Msg.Channel.SendMessageAsync(ErrorMsg);
+            var Result = await CommandService.ExecuteAsync(Context, argPos, Provider, MultiMatchHandling.Best);
         }
     }
 }
