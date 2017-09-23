30e8a2a61df4005093a115442c461220ac026c0e
Yivlx <vuxdev@gmail.com>
2017-09-22 17:40:10 -0700

    Add everything to DI

--- Core.cs
+++ Core.cs
@@ -1,12 +1,10 @@
-﻿# pragma warning disable 1998, 4014
+﻿# pragma warning disable 4014
 
-using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
-using Discord.Addons.Interactive;
 using Valerie.Handlers;
 using Valerie.Handlers.Server;
 using Valerie.Handlers.Config;
@@ -15,51 +13,34 @@ namespace Valerie
 {
     class Core
     {
-        static void Main(string[] args) => new Core().StartAsync().GetAwaiter().GetResult();
-        DiscordSocketClient Client;
-        CommandHandler CommandHandler;
+        static void Main(string[] args) => new Core().InitializeAsync().GetAwaiter().GetResult();
 
-        async Task StartAsync()
+        async Task InitializeAsync()
         {
-            await MainHandler.GetReadyAsync().ConfigureAwait(false);
-
-            Client = new DiscordSocketClient(new DiscordSocketConfig
-            {
-                AlwaysDownloadUsers = true,
-                LogLevel = LogSeverity.Info
-            });
-
-            CommandHandler = new CommandHandler(IServiceProvider());
-            await CommandHandler.ConfigureCommandsAsync();
-            EventsHandler.Provider = IServiceProvider();
-
-            Client.Log += EventsHandler.Log;
-            Client.JoinedGuild += EventsHandler.JoinedGuild;
-            Client.LeftGuild += EventsHandler.LeftGuild;
-            Client.GuildAvailable += EventsHandler.GuildAvailable;
-            Client.UserJoined += EventsHandler.UserJoined;
-            Client.UserLeft += EventsHandler.UserLeft;
-            Client.MessageReceived += EventsHandler.MessageReceivedAsync;
-            Client.LatencyUpdated += (Older, Newer) => EventsHandler.LatencyUpdated(Client, Older, Newer);
-            Client.Ready += async () => EventsHandler.ReadyAsync(Client);
-
-            await Client.LoginAsync(TokenType.Bot, BotConfig.Config.Token);
-            await Client.StartAsync();
-            await Task.Delay(-1);
-        }
-
-        IServiceProvider IServiceProvider()
-        {
-            return new ServiceCollection()
-                .AddSingleton(Client)
-                .AddSingleton<ServerConfig>()
-                .AddSingleton<InteractiveService>()
+            var Services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    AlwaysDownloadUsers = true,
+                    LogLevel = LogSeverity.Info
+                }))
                 .AddSingleton(new CommandService(new CommandServiceConfig
                 {
-                    ThrowOnError = false,
-                    DefaultRunMode = RunMode.Async
+                    DefaultRunMode = RunMode.Async,
+                    LogLevel = LogSeverity.Error,
+                    ThrowOnError = false
                 }))
-                .BuildServiceProvider();
+                .AddSingleton<CommandHandler>()
+                .AddSingleton<BotConfig>()
+                .AddSingleton<MainHandler>()
+                .AddSingleton<ServerConfig>()
+                .AddSingleton<EventsHandler>();
+
+            var Provider = Services.BuildServiceProvider();
+            Provider.GetRequiredService<MainHandler>().StartAsync(Provider);
+            Provider.GetRequiredService<CommandHandler>().InitializeAsync(Provider);
+            Provider.GetRequiredService<EventsHandler>();
+
+            await Task.Delay(-1);
         }
     }
 }
