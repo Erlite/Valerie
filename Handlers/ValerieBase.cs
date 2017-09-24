3a690c7d1fd4dfcb4e388e76c2fa8dea43750b7e
Yivlx <vuxdev@gmail.com>
2017-09-24 12:41:53 -0700

    Proper Check I guess?

--- Handlers/ValerieBase.cs
+++ Handlers/ValerieBase.cs
@@ -17,7 +17,7 @@ namespace Valerie.Handlers
             _ = Provider.GetRequiredService<ServerConfig>().SaveAsync(Context.Config, Context.Guild.Id);
             _ = Provider.GetRequiredService<BotConfig>().SaveAsync(Context.ValerieConfig);
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
-            if (message != null || embed != null)
+            if (!string.IsNullOrWhiteSpace(message) || embed != null)
                 return await Context.Channel.SendMessageAsync(message, isTTS, embed, options);
             else
                 return await Context.Channel.SendMessageAsync("Whoops, something went wrong.");
