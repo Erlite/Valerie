c12d998018ccdc36db041eff6be2952cf5e4383f
Yivlx <vuxdev@gmail.com>
2017-09-23 19:32:55 -0700

    Remove CommandService from MainHandler

--- Handlers/MainHandler.cs
+++ Handlers/MainHandler.cs
@@ -1,8 +1,7 @@
-﻿# pragma warning disable 4014
+﻿# pragma warning disable 4014, 1998
 using System;
 using System.Threading.Tasks;
 using Discord.WebSocket;
-using Discord.Commands;
 using Tweetinvi;
 using Tweetinvi.Exceptions;
 using Valerie.Services;
@@ -13,23 +12,20 @@ namespace Valerie.Handlers
 {
     class MainHandler
     {
-        IServiceProvider Provider;
         readonly BotConfig Config;
         readonly DiscordSocketClient Client;
         readonly EventsHandler EventsHandler;
-        readonly CommandService CommandService;
 
-        public MainHandler(DiscordSocketClient SocketClient, BotConfig BotConfig, EventsHandler Events, CommandService Commands)
+        public MainHandler(DiscordSocketClient SocketClient, BotConfig BotConfig, EventsHandler Events)
         {
             Client = SocketClient;
             Config = BotConfig;
             EventsHandler = Events;
-            CommandService = Commands;
         }
 
         static Lazy<IDocumentStore> DocumentStore = new Lazy<IDocumentStore>(new DocumentStore()
         {
-            Database = "Alpha",
+            Database = "Valerie",
             Urls = new string[]
             {
                 "http://localhost:8080"
@@ -40,33 +36,34 @@ namespace Valerie.Handlers
 
         public async Task StartAsync(IServiceProvider IServiceProvider)
         {
-            Provider = IServiceProvider;
-
+            ValerieBase<ValerieContext>.Provider = IServiceProvider;
             Logger.PrintInfo();
             await Config.LoadConfigAsync();
 
-            var TwitterConfig = Config.Config.APIKeys.TwitterKeys;
+            var TwitterConfig = BotConfig.Config.APIKeys.TwitterKeys;
             try
             {
                 Auth.SetUserCredentials(TwitterConfig.ConsumerKey, TwitterConfig.ConsumerSecret, TwitterConfig.AccessToken, TwitterConfig.AccessTokenSecret);
             }
             catch (TwitterInvalidCredentialsException E)
             {
-                Logger.Write(Logger.Status.ERR, Logger.Source.Config, E.Message);
+                Logger.Write(Status.ERR, Source.Config, E.Message);
             }
 
-            CommandService.Log += EventsHandler.Log;
             Client.Log += EventsHandler.Log;
             Client.JoinedGuild += EventsHandler.JoinedGuild;
             Client.LeftGuild += EventsHandler.LeftGuild;
             Client.GuildAvailable += EventsHandler.GuildAvailable;
             Client.UserJoined += EventsHandler.UserJoined;
             Client.UserLeft += EventsHandler.UserLeft;
+            Client.UserBanned += EventsHandler.UserBanned;
             Client.MessageReceived += EventsHandler.MessageReceivedAsync;
+            Client.ReactionAdded += EventsHandler.ReactionAddedAsync;
+            Client.ReactionRemoved += EventsHandler.ReactionRemovedAsync;
             Client.LatencyUpdated += (Older, Newer) => EventsHandler.LatencyUpdated(Client, Older, Newer);
             Client.Ready += async () => EventsHandler.ReadyAsync(Client);
 
-            await Client.LoginAsync(Discord.TokenType.Bot, Config.Config.Token);
+            await Client.LoginAsync(Discord.TokenType.Bot, BotConfig.Config.Token);
             await Client.StartAsync();
         }
     }
