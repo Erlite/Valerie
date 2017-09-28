212865199d876261c7e591eafa8c8550ee7d7977
Yivlx <vuxdev@gmail.com>
2017-09-27 19:50:52 -0700

    Todo #0

--- Modules/OwnerModule.cs
+++ Modules/OwnerModule.cs
@@ -203,7 +203,7 @@ namespace Valerie.Modules
 
     public class Globals
     {
-        public ICommandContext Context { get; internal set; }
+        public ValerieContext Context { get; internal set; }
         public DiscordSocketClient Client { get; internal set; }
         public SocketGuildUser User { get; internal set; }
         public SocketGuild Guild { get; internal set; }
