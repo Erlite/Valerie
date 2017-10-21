dba7a053da225c3052068dc894f223a31c8b0148
Yivlx <vuxdev@gmail.com>
2017-10-21 14:49:14 -0700

    :wink:

--- Extensions/IntExtension.cs
+++ Extensions/IntExtension.cs
@@ -4,11 +4,7 @@ namespace Valerie.Extensions
 {
     public class IntExtension
     {
-        public static int GiveEridium(int EridiumToGive, int TotalUsers)
-        {
-            int GetTotal = (TotalUsers > 500) ? TotalUsers : (int)Math.Sqrt(TotalUsers);
-            return (EridiumToGive < 500) ? EridiumToGive : EridiumToGive / GetTotal;
-        }
+        public static int GiveEridium(int EridiumToGive) => EridiumToGive / (int)Math.Sqrt(DateTime.Now.Millisecond);
 
         public static int GetLevel(int Eridium) => 1 + (int)Math.Pow(Eridium, 1 / 4.0);
 
