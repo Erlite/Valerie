ab6332820670cfa238c0a5c554f8bf687e6dfdf4
Yivlx <vuxdev@gmail.com>
2017-10-21 14:49:37 -0700

    Proper session handler

--- Handlers/ValerieBase.cs
+++ Handlers/ValerieBase.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.Commands;
-using Valerie.Handlers.Server;
-using Valerie.Handlers.Config;
 using Raven.Client.Documents.Session;
 
 namespace Valerie.Handlers
@@ -39,16 +36,10 @@ namespace Valerie.Handlers
         }
 
         public async Task ReactAsync(string GetEmoji)
-        {
-            await Provider.GetRequiredService<ServerConfig>().SaveAsync(Context.Config, Context.Guild.Id).ConfigureAwait(false);
-            await Provider.GetRequiredService<BotConfig>().SaveAsync(Context.ValerieConfig).ConfigureAwait(false);
-            await Context.Message.AddReactionAsync(new Emoji(GetEmoji)).ConfigureAwait(false);
-        }
+            => await Context.Message.AddReactionAsync(new Emoji(GetEmoji)).ConfigureAwait(false);
 
         public async Task<IUserMessage> SendMessageAsync(ITextChannel Channel, string Message)
         {
-            _ = Provider.GetRequiredService<ServerConfig>().SaveAsync(Context.Config, Context.Guild.Id);
-            _ = Provider.GetRequiredService<BotConfig>().SaveAsync(Context.ValerieConfig);
             await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
             return await Channel.SendMessageAsync(Message).ConfigureAwait(false);
         }
