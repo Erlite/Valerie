10adcbae366ec86befcfebdcca53b6593b456701
Yivlx <vuxdev@gmail.com>
2017-10-27 14:27:56 -0700

    Let's not do that.

--- Handlers/Config/Models/ConfigModel.cs
+++ Handlers/Config/Models/ConfigModel.cs
@@ -14,8 +14,8 @@ namespace Valerie.Handlers.Config.Models
         public string CensoredWords { get; set; } =
             "\\\b(f+u+c+k+|b+i+t+c+h+|w+h+o+r+e+|c+u+n+t+|a+ss+h+o+l+e+|a+s+s+|n+i+g+g+e+r+|d+i+c+k+|p+u+s+s+y+|f+a+g+g+o+t+)(w+i+t+|e+r+|i+n+g+)?\\\b";
         public KeysModel APIKeys { get; set; } = new KeysModel();
-        public List<string> EvalImports { get; set; } = new List<string>() { "System", "System.Collections.Generic", "System.Linq" };
-        public List<string> BotGames { get; set; } = new List<string>() { "With You", "Tumblr" };
+        public List<string> EvalImports { get; set; } = new List<string>();
+        public List<string> BotGames { get; set; } = new List<string>();
         public ConcurrentDictionary<ulong, string> UsersBlacklist { get; set; } = new ConcurrentDictionary<ulong, string>();
         public List<ulong> PokeServers = new List<ulong>();
     }
