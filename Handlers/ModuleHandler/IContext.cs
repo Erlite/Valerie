9b1833754d26b69332056f74e8798a29a7d05e44
Yivlx <vuxdev@gmail.com>
2017-11-07 19:15:37 -0800

    Lite Code

---
+++ Handlers/ModuleHandler/IContext.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Valerie.Handlers.ModuleHandler
+{
+    public class IContext : ICommandContext
+    {
+        public IUser User { get; }
+        public IGuild Guild { get; }
+        public ConfigModel Config { get; }
+        public ServerModel Server { get; }
+        public IDiscordClient Client { get; }
+        public IUserMessage Message { get; }
+        public IMessageChannel Channel { get; }
+        public ConfigHandler ConfigHandler { get; }
+        public ServerHandler ServerHandler { get; }
+
+        public IContext(IDiscordClient ClientParam, IUserMessage MessageParam, IServiceProvider ServiceProvider)
+        {
+            Client = ClientParam;
+            Message = MessageParam;
+            User = MessageParam.Author;
+            Channel = MessageParam.Channel;
+            Guild = (MessageParam.Channel as IGuildChannel).Guild;
+            ConfigHandler = ServiceProvider.GetRequiredService<ConfigHandler>();
+            ServerHandler = ServiceProvider.GetRequiredService<ServerHandler>();
+            Config = ServiceProvider.GetRequiredService<ConfigHandler>().GetConfigAsync().GetAwaiter().GetResult();
+            Server = ServiceProvider.GetRequiredService<ServerHandler>().GetServerAsync(Guild.Id).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Handlers/ModuleHandler/ValerieBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Valerie.Handlers.ModuleHandler
+{
+    public class ValerieBase : ModuleBase<IContext>
+    {
+        public async Task<IUserMessage> ReplyAsync(string Message, Embed GetEmbed = null)
+        {
+            await Context.Channel.TriggerTypingAsync();
+            return await base.ReplyAsync(Message, false, GetEmbed, null);
+        }
+
+        public async Task<IUserMessage> SaveAsync(string Message = null)
+        {
+            var check = await Context.ServerHandler.UpdateServerAsync(Context.Guild.Id, Context.Server).ConfigureAwait(false);
+            if (check == true) return await ReplyAsync(Message ?? "✔️ - Done.");
+            return await ReplyAsync(Message ?? "✖️ - There was an error.");
+        }
+    }
+}
