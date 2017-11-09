8cb3ff1757cb807d33b96d74a5554dcae345cc0a
Yivlx <vuxdev@gmail.com>
2017-11-08 19:16:44 -0800

    Tag Module Updated

--- Modules/TagModule.cs
+++ Modules/TagModule.cs
@@ -1,10 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Valerie.Handlers.ModuleHandler;
 using Models;
-using System.Collections.Generic;
-using System;
 
 namespace Valerie.Modules
 {
@@ -15,16 +15,15 @@ namespace Valerie.Modules
         public Task TagAsync(string TagName)
         {
             var Tag = Context.Server.Tags.FirstOrDefault(x => x.Name == TagName);
-            if (!Exists(TagName)) return Task.CompletedTask;
+            if (!NotExists(TagName)) return Task.CompletedTask;
             Context.Server.Tags.FirstOrDefault(x => x.Name == TagName).Uses++;
             return SaveAsync(Tag.Response);
         }
 
-        [Command("Create"), Summary("Creates a new tag for this server."), Priority(1)]
+        [Command("Create"), Alias("Make", "New", "Add"), Summary("Creates a new tag for this server."), Priority(1)]
         public Task CreateAsync(string Name, string Response)
         {
-            if (NotExists(Name))
-                return Task.CompletedTask;
+            if (Exists(Name)) return Task.CompletedTask;
             Context.Server.Tags.Add(new TagWrapper
             {
                 CreationDate = DateTime.Now,
@@ -36,15 +35,58 @@ namespace Valerie.Modules
             return SaveAsync();
         }
 
+        [Command("Modify"), Alias("Change", "Update"), Summary("Updates an existing tag"), Priority(1)]
+        public Task ModifyAsync(string Name, string Response)
+        {
+            if (!NotExists(Name)) return Task.CompletedTask;
+            var Tag = Context.Server.Tags.FirstOrDefault(x => x.Name == Name);
+            if (Convert.ToUInt64(Tag.Owner) != Context.User.Id)
+                return ReplyAsync($"You are not the owner of tag `{Name}`.");
+            Context.Server.Tags.FirstOrDefault(x => x.Name == Name).Response = Response;
+            return SaveAsync();
+        }
 
-        bool NotExists(string Name)
+        [Command("Delete"), Alias("Remove"), Summary("Deletes a tag."), Priority(1)]
+        public Task DeleteAsync(string Name)
+        {
+            if (!NotExists(Name)) return Task.CompletedTask;
+            var Tag = Context.Server.Tags.FirstOrDefault(x => x.Name == Name);
+            if (Convert.ToUInt64(Tag.Owner) != Context.User.Id || Context.Server.Admins.Contains(Context.User.Id))
+                return ReplyAsync($"You are not the owner of tag `{Name}`.");
+            Context.Server.Tags.Remove(Tag);
+            return SaveAsync();
+        }
+
+        [Command("User"), Summary("Shows all tags owned by you or a given user."), Priority(1)]
+        public Task UserAsync(IGuildUser User = null)
+        {
+            User = User ?? Context.User as IGuildUser;
+            var UserTag = Context.Server.Tags.Where(x => x.Owner == $"{User.Id}");
+            if (!Context.Server.Tags.Any() || !UserTag.Any()) return ReplyAsync($"{User} doesn't have any tags.");
+            return ReplyAsync($"{User} owns {UserTag.Count()} tags.\n```{string.Join(", ", UserTag.Select(x => x.Name))}```");
+        }
+
+        [Command("Info"), Alias("About"), Summary("Displays information about a given tag."), Priority(1)]
+        public async Task InfoAsync(string Name)
+        {
+            if (NotExists(Name)) return;
+            var Tag = Context.Server.Tags.FirstOrDefault(x => x.Name == Name);
+            await ReplyAsync($"```" +
+                $"Name     :  {Tag.Name}\n" +
+                $"Owner   :   {(await Context.Guild.GetUserAsync(Convert.ToUInt64(Tag.Owner))).Username ?? "Unkown User."}\n" +
+                $"Uses      :  {Tag.Uses}\n" +
+                $"Created At   :   {Tag.CreationDate}\n" +
+                $"Response    :   {Tag.Response}\n```");
+        }
+
+        bool Exists(string Name)
         {
             if (Context.Server.Tags.FirstOrDefault(x => x.Name == Name) == null) return false;
             _ = ReplyAsync($"Tag `{Name}` already exists.");
             return true;
         }
 
-        bool Exists(string Name)
+        bool NotExists(string Name)
         {
             if (Context.Server.Tags.FirstOrDefault(x => x.Name == Name) != null) return true;
             _ = SuggestAsync(Name);
