836af3625c5a5a2b58bd69e617132d9ad474198d
Yivlx <vuxdev@gmail.com>
2017-11-09 19:29:30 -0800

    Custom Precondition attribute

---
+++ Attributes/RequireAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Valerie.Handlers.ModuleHandler;
+
+namespace Valerie.Attributes
+{
+    public class RequireAccess : PreconditionAttribute
+    {
+        AccessLevel GetAccessLevel;
+        public RequireAccess(AccessLevel Level) => GetAccessLevel = Level;
+
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo Command, IServiceProvider Services)
+        {
+            var Context = (context as IContext);
+            var User = Context.User as IGuildUser;
+            var AdminPerms = (User.Id == Context.Guild.OwnerId || User.GuildPermissions.Administrator || Context.Server.Admins.Contains(User.Id));
+            var ModPerms = (User.GuildPermissions.KickMembers || User.GuildPermissions.BanMembers);
+            if (GetAccessLevel == AccessLevel.Admins && AdminPerms)
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            else if (GetAccessLevel == AccessLevel.Mods && ModPerms)
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            else if (GetAccessLevel == AccessLevel.AdminsNMods && (AdminPerms || ModPerms))
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            else
+                return Task.FromResult(PreconditionResult.FromError($"{Command.Name} requires {typeof(AccessLevel).Name} access."));
+        }
+    }
+
+    public enum AccessLevel
+    {
+        Admins,
+        Mods,
+        AdminsNMods
+    }
+}
