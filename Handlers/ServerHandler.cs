dfef8f4adecb70a011e1193eedb4463305f98e0c
Yivlx <vuxdev@gmail.com>
2017-11-09 19:29:59 -0800

    Catch error and change log source

--- Handlers/ServerHandler.cs
+++ Handlers/ServerHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Models;
 using Valerie.Services;
+using System;
 
 namespace Valerie.Handlers
 {
@@ -14,36 +15,52 @@ namespace Valerie.Handlers
             var Load = await ServerListAsync().ConfigureAwait(false);
             if (Load.Any(x => x.Id == Server.Id)) return;
             var Get = await MainHandler.RestServer.AddServerAsync(Server).ConfigureAwait(false);
-            if (!Get.IsSuccessStatusCode) LogClient.Write(Source.SERVER, $"There was an error adding server: {Server.Id}");
+            if (!Get.IsSuccessStatusCode) LogClient.Write(Source.REST, $"There was an error adding server: {Server.Id}");
         }
 
         public async Task DeleteServerAsync(ulong Id)
         {
             var Get = await MainHandler.RestServer.DeleteServerAsync(Id).ConfigureAwait(false);
-            if (!Get.IsSuccessStatusCode) LogClient.Write(Source.SERVER, $"There was an error deleting server: {Id}");
+            if (!Get.IsSuccessStatusCode) LogClient.Write(Source.REST, $"There was an error deleting server: {Id}");
         }
 
         public async Task<bool> UpdateServerAsync(ulong Id, ServerModel Server)
         {
             var TryUpdate = await MainHandler.RestServer.UpdateServerAsync(Id, Server).ConfigureAwait(false);
-            if (!TryUpdate.IsSuccessStatusCode) LogClient.Write(Source.SERVER, $"Failed to update server: {Id}");
+            if (!TryUpdate.IsSuccessStatusCode) LogClient.Write(Source.REST, $"Failed to update server: {Id}");
             return true;
         }
 
         public async Task<IReadOnlyCollection<ServerModel>> ServerListAsync()
         {
-            var Get = await MainHandler.RestServer.ServerListAsync().ConfigureAwait(false);
-            if (!Get.IsSuccessStatusCode) { }
-            var Content = await Get.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<IReadOnlyCollection<ServerModel>>(Content);
+            try
+            {
+                var Get = await MainHandler.RestServer.ServerListAsync().ConfigureAwait(false);
+                if (!Get.IsSuccessStatusCode) { LogClient.Write(Source.REST, Get.ReasonPhrase); };
+                var Content = await Get.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return JsonConvert.DeserializeObject<IReadOnlyCollection<ServerModel>>(Content);
+            }
+            catch(Exception Ex)
+            {
+                LogClient.Write(Source.SERVER, $"-> {Ex.Message}\n{Ex.StackTrace}");
+                return await Task.FromException<IReadOnlyCollection<ServerModel>>(Ex);
+            }
         }
 
         public async Task<ServerModel> GetServerAsync(ulong Id)
         {
-            var Get = await MainHandler.RestServer.GetServerAsync(Id).ConfigureAwait(false);
-            if (!Get.IsSuccessStatusCode) LogClient.Write(Source.SERVER, $"Failed to get server: {Id}");
-            var Content = await Get.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return JsonConvert.DeserializeObject<ServerModel>(Content);
+            try
+            {
+                var Get = await MainHandler.RestServer.GetServerAsync(Id).ConfigureAwait(false);
+                if (!Get.IsSuccessStatusCode) LogClient.Write(Source.REST, $"Failed to get server: {Id}");
+                var Content = await Get.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return JsonConvert.DeserializeObject<ServerModel>(Content);
+            }
+            catch (Exception Ex)
+            {
+                LogClient.Write(Source.SERVER, $"-> {Ex.Message}\n{Ex.StackTrace}");
+                return await Task.FromException<ServerModel>(Ex);
+            }
         }
     }
 }
