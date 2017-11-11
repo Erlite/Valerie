389e3db00d7546370410e9b53ddb9727b5ce9691
Yivlx <vuxdev@gmail.com>
2017-11-10 19:04:08 -0800

    Modified RequireNsfw Attribute

---
+++ Attributes/RequireNsfw.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Valerie.Attributes
+{
+    public class RequireNsfw : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
+        {
+            var Chn = Context.Channel as Discord.ITextChannel;
+            if (Chn.IsNsfw || Chn.Name.Contains("nsfw"))
+                return Task.FromResult(PreconditionResult.FromSuccess());
+            else
+                return Task.FromResult(PreconditionResult.FromError($"**{Info.Name}** command can only be ran in NSFW channel, pervert."));
+        }
+    }
+}
