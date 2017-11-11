a3a68faa8f706eb500ee6540fd67daecd833d59f
Yivlx <vuxdev@gmail.com>
2017-11-10 19:04:25 -0800

    Added HttpClient and Random to IContext

--- Handlers/ModuleHandler/IContext.cs
+++ Handlers/ModuleHandler/IContext.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 using System;
 using Models;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace Valerie.Handlers.ModuleHandler
 {
@@ -10,9 +11,11 @@ namespace Valerie.Handlers.ModuleHandler
     {
         public IUser User { get; }
         public IGuild Guild { get; }
+        public Random Random { get; }
         public ConfigModel Config { get; }
         public ServerModel Server { get; }
         public IDiscordClient Client { get; }
+        public HttpClient HttpClient { get; }
         public IUserMessage Message { get; }
         public IMessageChannel Channel { get; }
         public ConfigHandler ConfigHandler { get; }
@@ -25,6 +28,8 @@ namespace Valerie.Handlers.ModuleHandler
             User = MessageParam.Author;
             Channel = MessageParam.Channel;
             Guild = (MessageParam.Channel as IGuildChannel).Guild;
+            Random = ServiceProvider.GetRequiredService<Random>();
+            HttpClient = ServiceProvider.GetRequiredService<HttpClient>();
             ConfigHandler = ServiceProvider.GetRequiredService<ConfigHandler>();
             ServerHandler = ServiceProvider.GetRequiredService<ServerHandler>();
             Config = ServiceProvider.GetRequiredService<ConfigHandler>().GetConfigAsync().GetAwaiter().GetResult();
