39b888091e2d2fb3ad6fc5842320b36046dbdf18
Yivlx <vuxdev@gmail.com>
2017-11-10 19:05:02 -0800

    Porn Model for Porn Command

---
+++ JsonModels/PornModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+namespace Valerie.JsonModels
+{
+    public class PornModel
+    {
+        [JsonProperty("result")]
+        public Result[] Result { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonProperty("thumb")]
+        public string Thumb { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("views")]
+        public long Views { get; set; }
+    }
+}
