597fd99d1912d2a994b60734ef87c66ab95a6603
Yivlx <vuxdev@gmail.com>
2017-11-10 19:04:48 -0800

    More Abstractions

--- Handlers/ModuleHandler/ValerieBase.cs
+++ Handlers/ModuleHandler/ValerieBase.cs
@@ -1,16 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Models;
 
 namespace Valerie.Handlers.ModuleHandler
 {
     public class ValerieBase : ModuleBase<IContext>
     {
-        public async Task<IUserMessage> ReplyAsync(string Message, Embed GetEmbed = null)
+        public async Task<IUserMessage> ReplyAsync(string Message)
         {
-            await Context.ServerHandler.UpdateServerAsync(Context.Guild.Id, Context.Server).ConfigureAwait(false);
             await Context.Channel.TriggerTypingAsync();
-            return await base.ReplyAsync(Message, false, GetEmbed, null);
+            return await base.ReplyAsync(Message, false, null, null);
         }
 
         public async Task<IUserMessage> SaveAsync(string Message = null)
@@ -18,6 +19,31 @@ namespace Valerie.Handlers.ModuleHandler
             var check = await Context.ServerHandler.UpdateServerAsync(Context.Guild.Id, Context.Server).ConfigureAwait(false);
             if (check == true) return await ReplyAsync(Message ?? "✅ - Done.");
             return await ReplyAsync(Message ?? "✖️ - There was an error.");
+        }
+
+        public async Task LogAsync(IGuildUser User, CaseType CaseType, string Reason)
+        {
+            var ModChannel = await Context.Guild.GetTextChannelAsync(Convert.ToUInt64(Context.Server.ModLog.TextChannel));
+            if (ModChannel == null) return;
+            Reason = Reason ?? $"*Responsible moderator, please type `{Context.Config.Prefix}Reason {Context.Server.ModLog.ModCases.Count + 1} <Reason>`*";
+            var Message = await ModChannel.SendMessageAsync($"**{typeof(CaseType).Name}** | Case {Context.Server.ModLog.ModCases.Count + 1}\n**User:** {User} ({User.Id})\n**Reason:** {Reason}\n" +
+                    $"**Responsible Moderator:** {Context.User}");
+            Context.Server.ModLog.ModCases.Add(new CaseWrapper()
+            {
+                Reason = Reason,
+                CaseType = CaseType,
+                MessageId = $"{Message.Id}",
+                ResponsibleMod = $"{Context.User}",
+                UserInfo = $"{User.Username} ({User.Id})",
+                CaseNumber = Context.Server.ModLog.ModCases.Count + 1
+            });
+            await Context.ServerHandler.UpdateServerAsync(Context.Guild.Id, Context.Server).ConfigureAwait(false);
+        }
+
+        public async Task<IUserMessage> SendEmbedAsync(Embed Embed)
+        {
+            await Context.ServerHandler.UpdateServerAsync(Context.Guild.Id, Context.Server).ConfigureAwait(false);
+            return await base.ReplyAsync(null, false, Embed);
         }
     }
 }
