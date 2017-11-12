172d0fbb68d8d8cda96fb81f28b794427e08a566
Yivlx <vuxdev@gmail.com>
2017-11-12 15:15:40 -0800

    Rewrite Completed for Admin Commands.

--- Modules/AdminModule.cs
+++ Modules/AdminModule.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Valerie.Attributes;
+using Valerie.Extensions;
 using Valerie.Handlers.ModuleHandler;
 
 namespace Valerie.Modules
@@ -51,18 +53,20 @@ namespace Valerie.Modules
             return SaveAsync();
         }
 
-        [Command("SelfRole"), Alias("SlfR"), Summary("Adds/Removes role to/from self assingable roles. Action: Add, Remove")]
+        [Command("SelfRoles"), Alias("SlfR"), Summary("Adds/Removes role to/from self assingable roles. Action: Add, Remove")]
         public Task SelfRoleAsync(ModuleEnums Action, IRole Role)
         {
             switch (Action)
             {
                 case ModuleEnums.Add:
-                    if (Context.Server.AssignableRoles.Contains($"{Role.Id}")) return ReplyAsync($"{Role.Name} already exists in assignable roles.");
-                    Context.Server.AssignableRoles.Add($"{Role.Id}");
+                    if (Context.Server.AssignableRoles.Contains(Role.Id)) return ReplyAsync($"{Role.Name} already exists in assignable roles.");
+                    else if (Context.Server.AssignableRoles.Count == Context.Server.AssignableRoles.Capacity)
+                        return ReplyAsync($"It seems you have reached max number of assignable roles.");
+                    Context.Server.AssignableRoles.Add(Role.Id);
                     return SaveAsync();
                 case ModuleEnums.Remove:
-                    if (!Context.Server.AssignableRoles.Contains($"{Role.Id}")) return ReplyAsync($"{Role.Name} doesn't exists in assignable roles.");
-                    Context.Server.AssignableRoles.Remove($"{Role.Id}");
+                    if (!Context.Server.AssignableRoles.Contains(Role.Id)) return ReplyAsync($"I couldn't find  {Role.Name} role.");
+                    Context.Server.AssignableRoles.Remove(Role.Id);
                     return SaveAsync();
             }
             return Task.CompletedTask;
@@ -75,7 +79,7 @@ namespace Valerie.Modules
             switch (Action)
             {
                 case ModuleEnums.Add:
-                    if (Context.Server.JoinMessages.Count == 5) return ReplyAsync("Can't have more than 5 join messages.");
+                    if (Context.Server.JoinMessages.Count == Context.Server.JoinMessages.Capacity) return ReplyAsync("You have reached max number of join messages.");
                     else if (Context.Server.JoinMessages.Contains(Message)) return ReplyAsync("Join message already exists. Try something different?");
                     Context.Server.JoinMessages.Add(Message);
                     return SaveAsync();
@@ -94,7 +98,7 @@ namespace Valerie.Modules
             switch (Action)
             {
                 case ModuleEnums.Add:
-                    if (Context.Server.LeaveMessages.Count == 5) return ReplyAsync("Can't have more than 5 leave messages.");
+                    if (Context.Server.LeaveMessages.Count == Context.Server.LeaveMessages.Capacity) return ReplyAsync("You have reached max number of leave messages.");
                     else if (Context.Server.LeaveMessages.Contains(Message)) return ReplyAsync("Leave message already exists. Try something different?");
                     Context.Server.LeaveMessages.Add(Message);
                     return SaveAsync();
@@ -123,14 +127,14 @@ namespace Valerie.Modules
             return Task.CompletedTask;
         }
 
-        [Command("Admins"), Summary("Adds/Removes users from server's admins. Admins are able to change bot's server configuration.")]
+        [Command("Admins"), Summary("Adds/Removes users from server's admins. Action: Add, Remove. Admins are able to change bot's server configuration.")]
         public Task AdminsAsync(ModuleEnums Action, IGuildUser User)
         {
             switch (Action)
             {
                 case ModuleEnums.Add:
                     if (Context.Server.Admins.Contains(User.Id)) return ReplyAsync($"{User} is already an Admin.");
-                    else if (Context.Server.Admins.Count >= 10) return ReplyAsync($"Can't have more than 10 Admins.");
+                    else if (Context.Server.Admins.Count == Context.Server.Admins.Capacity) return ReplyAsync($"It seems you have reached max number of admins.");
                     Context.Server.Admins.Add(User.Id);
                     return SaveAsync();
                 case ModuleEnums.Remove:
@@ -141,11 +145,145 @@ namespace Valerie.Modules
             return Task.CompletedTask;
         }
 
-        [Command("XpForbid"), Summary("Forbids a role from gaining XP.")]
+        [Command("XpForbid"), Summary("Forbids a role from gaining XP. Action: Add, Remove")]
         public Task ForbidAsync(ModuleEnums Action, IRole Role)
         {
+            switch (Action)
+            {
+                case ModuleEnums.Add:
+                    if (Context.Server.ChatXP.ForbiddenRoles.Contains(Role.Id)) return ReplyAsync($"{Role} already is forbidden from gaining XP.");
+                    else if (Context.Server.ChatXP.ForbiddenRoles.Count == Context.Server.ChatXP.ForbiddenRoles.Capacity)
+                        return ReplyAsync("It seems you have reached max number of forbidden roles.");
+                    Context.Server.ChatXP.ForbiddenRoles.Add(Role.Id);
+                    return SaveAsync();
+                case ModuleEnums.Remove:
+                    if (!Context.Server.ChatXP.ForbiddenRoles.Contains(Role.Id)) return ReplyAsync($"{Role} isn't forbidden from gaining XP.");
+                    Context.Server.ChatXP.ForbiddenRoles.Remove(Role.Id);
+                    return SaveAsync();
+            }
             return Task.CompletedTask;
         }
 
+        [Command("XpLevel"), Summary("Adds/Removes a level up role. Action: Add, Remove, Modify")]
+        public Task LevelAsync(ModuleEnums Action, IRole Role, int Level = 10)
+        {
+            switch (Action)
+            {
+                case ModuleEnums.Add:
+                    if (Context.Server.ChatXP.LevelRoles.Count == 15) return ReplyAsync("You have reached max number of level up roles.");
+                    else if (Context.Server.ChatXP.LevelRoles.ContainsKey(Role.Id)) return ReplyAsync($"{Role} is already a level-up role.");
+                    Context.Server.ChatXP.LevelRoles.Add(Role.Id, Level);
+                    return SaveAsync();
+                case ModuleEnums.Remove:
+                    if (!Context.Server.ChatXP.LevelRoles.ContainsKey(Role.Id)) return ReplyAsync($"{Role} isn't a level-up role.");
+                    Context.Server.ChatXP.LevelRoles.Remove(Role.Id);
+                    return SaveAsync();
+                case ModuleEnums.Modify:
+                    if (!Context.Server.ChatXP.LevelRoles.ContainsKey(Role.Id)) return ReplyAsync($"{Role} isn't a level-up role.");
+                    Context.Server.ChatXP.LevelRoles[Role.Id] = Level;
+                    return SaveAsync();
+            }
+            return Task.CompletedTask;
+        }
+
+        [Command("Badwords"), Summary("Add/remove bad words. If a user says a bad word their message will be deleted. Action: Add, Remove")]
+        public Task BadWordsAsync(ModuleEnums Action, string Badword)
+        {
+            switch (Action)
+            {
+                case ModuleEnums.Add:
+                    if (Context.Server.ModLog.BadWords.Count == Context.Server.ModLog.BadWords.Capacity)
+                        return ReplyAsync("You have reached max number of bad words.");
+                    else if (Context.Server.ModLog.BadWords.Contains(Badword)) return ReplyAsync($"{Badword} already exists.");
+                    Context.Server.ModLog.BadWords.Add(Badword);
+                    return SaveAsync();
+                case ModuleEnums.Remove:
+                    if (!Context.Server.ModLog.BadWords.Contains(Badword)) return ReplyAsync($"{Badword} doesn't exists.");
+                    Context.Server.ModLog.BadWords.Remove(Badword);
+                    return SaveAsync();
+            }
+            return Task.CompletedTask;
+        }
+
+        [Command("Blocklink"), Alias("Add/removes a link from blocked urls. Users will be warned for posting a blocked link. Action: Add, Remove")]
+        public Task BlocklinkAsync(ModuleEnums Action, string Url)
+        {
+            var Check = BoolExt.IsValidUrl(Url);
+            switch (Action)
+            {
+                case ModuleEnums.Add:
+                    if (!Check.Item1) return ReplyAsync($"{Url} isn't a valid Url.");
+                    else if (Context.Server.ModLog.BlockedUrls.Contains(Check.Item2)) return ReplyAsync($"{Url} already is blocked.");
+                    Context.Server.ModLog.BlockedUrls.Add(Check.Item2);
+                    return SaveAsync();
+                case ModuleEnums.Remove:
+                    if (!Context.Server.ModLog.BlockedUrls.Contains(Check.Item2)) return ReplyAsync($"{Url} isn't blocked.");
+                    Context.Server.ModLog.BlockedUrls.Remove(Check.Item2);
+                    return SaveAsync();
+            }
+            return Task.CompletedTask;
+        }
+
+        [Command("Show JoinMsgs"), Alias("Showjms"), Summary("Shows all the join messages for this server.")]
+        public Task ShowJoinMsgsAsync()
+        {
+            if (!Context.Server.JoinMessages.Any()) return ReplyAsync($"{Context.Guild} has no join messages.");
+            return ReplyAsync($"**Join Messages**\n{string.Join("\n", $"-> {Context.Server.JoinMessages}")}");
+        }
+
+        [Command("Show LeaveMsgs"), Alias("Showlms"), Summary("Shows all the join messages for this server.")]
+        public Task ShowLeaveMsgsAsync()
+        {
+            if (!Context.Server.LeaveMessages.Any()) return ReplyAsync($"{Context.Guild} has no leave messages.");
+            return ReplyAsync($"**Leave Messages**\n{string.Join("\n", $"-> {Context.Server.LeaveMessages}")}");
+        }
+
+        [Command("Show Admins"), Alias("ShowAs"), Summary("Shows all the current admins for this server.")]
+        public async Task ShowAdminsAsync()
+        {
+            if (!Context.Server.Admins.Any())
+            {
+                await ReplyAsync($"{Context.Guild} has no admins.");
+                return;
+            }
+            string Admins = null;
+            foreach (var Id in Context.Server.Admins)
+                Admins += $"\n-> {Id} | {await StringExt.CheckUserAsync(Context, Id)}";
+            await ReplyAsync($"**{Context.Server} Admins**\n{Admins}");
+        }
+
+        [Command("Show Forbidden"), Alias("Showfb"), Summary("Shows all the forbidden roles for this server.")]
+        public Task ShowForbiddenAsync()
+        {
+            if (!Context.Server.ChatXP.ForbiddenRoles.Any()) return ReplyAsync($"{Context.Server} has no forbidden roles.");
+            string Roles = null;
+            foreach (var Id in Context.Server.ChatXP.ForbiddenRoles)
+                Roles += $"-> {Id} | {StringExt.CheckRole(Context, Id)}";
+            return ReplyAsync($"**Forbidden Roles**\n{Roles}");
+        }
+
+        [Command("Show Levels"), Alias("Showlvls"), Summary("Shows all the level up roles for this server.")]
+        public Task ShowLevelsAsync()
+        {
+            if (!Context.Server.ChatXP.LevelRoles.Any()) return ReplyAsync($"{Context.Server} has no level-up roles.");
+            string Roles = null;
+            foreach (var Id in Context.Server.ChatXP.LevelRoles.Keys)
+                Roles += $"-> {Id} | {StringExt.CheckRole(Context, Id)}";
+            return ReplyAsync($"**Level-Up Roles**\n{Roles}");
+        }
+
+        [Command("Show Badwords"), Alias("Showbws"), Summary("Shows all the bad words for this server.")]
+        public Task ShowBadwordsAsync()
+        {
+            if (!Context.Server.ModLog.BadWords.Any()) return ReplyAsync($"{Context.Guild} has no prohibited words.");
+            return ReplyAsync($"**Bad Words**\n{string.Join(", ", Context.Server.ModLog.BadWords)}");
+        }
+
+        [Command("Show BlockedUrls"), Alias("Showbls"), Summary("Shows all the blocked links for this server.")]
+        public Task ShowBlockedUrlsAsync()
+        {
+            if (!Context.Server.ModLog.BlockedUrls.Any()) return ReplyAsync($"{Context.Guild} has no blocked urls.");
+            return ReplyAsync($"**Blocked Links**\n{string.Join(", ", Context.Server.ModLog.BlockedUrls)}");
+        }
     }
 }
