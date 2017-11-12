d367446279727f84e0d37c56c7e74c7e3902799d
Yivlx <vuxdev@gmail.com>
2017-11-12 15:14:51 -0800

    String Extension: Role & User check methods.

---
+++ Extensions/StringExt.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Valerie.Handlers.ModuleHandler;
+
+namespace Valerie.Extensions
+{
+    public class StringExt
+    {
+        public static async Task<string> CheckUserAsync(IContext Context, ulong Id)
+        {
+            var User = await Context.Guild.GetUserAsync(Id);
+            return User == null ? "Unknown User." : User.Username;
+        }
+
+        public static string CheckRole(IContext Context, ulong Id)
+        {
+            var Role = Context.Guild.GetRole(Id);
+            return Role == null ? "Unknown Role." : Role.Name;
+        }
+    }
+}
