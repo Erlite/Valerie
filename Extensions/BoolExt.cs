20741e6ecaf55254d144dca661cc3f05c3c230b2
Yivlx <vuxdev@gmail.com>
2017-11-12 15:15:02 -0800

    Url Checker.

---
+++ Extensions/BoolExt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Valerie.Extensions
+{
+    public static class BoolExt
+    {
+        public static (bool, Uri) IsValidUrl(this string Url)
+        {
+            bool UriCheck = Uri.TryCreate(Url, UriKind.Absolute, out Uri Result) &&
+                (Result.Scheme == Uri.UriSchemeHttp || Result.Scheme == Uri.UriSchemeHttps);
+            return UriCheck == true && Result != null ? (true, Result) : (false, null);
+        }
+    }
+}
