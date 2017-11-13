8f6f9f17a393d8673f1c252fdd8defec7c60f6a6
Yivlx <vuxdev@gmail.com>
2017-11-13 08:23:25 -0800

    Added GitModel

---
+++ JsonModels/GitModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Valerie.JsonModels
+{
+    public class GitModel
+    {
+        [JsonProperty("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+    }
+}
