6ae64df72f9c1035acc0588b6354dcc2ff80aab0
Yivlx <vuxdev@gmail.com>
2017-11-15 20:33:51 -0800

    Fix Json Error for Parsing Commit Message

--- JsonModels/GitModel.cs
+++ JsonModels/GitModel.cs
@@ -4,13 +4,19 @@ namespace Valerie.JsonModels
 {
     public class GitModel
     {
+        [JsonProperty("commit")]
+        public Commit Commit { get; set; }
+
         [JsonProperty("html_url")]
         public string HtmlUrl { get; set; }
 
-        [JsonProperty("message")]
-        public string Message { get; set; }
-
         [JsonProperty("sha")]
         public string Sha { get; set; }
+    }
+
+    public class Commit
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
     }
 }
