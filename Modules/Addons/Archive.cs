41e3f1825846e4b37d834c868275b6defa6f887a
Yivlx <vuxdev@gmail.com>
2017-11-15 20:31:26 -0800

    Globals And Archive Models for Commands.

---
+++ Modules/Addons/Archive.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Valerie.Modules.Addons
+{
+    public class Archive
+    {
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public string Attachments { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
