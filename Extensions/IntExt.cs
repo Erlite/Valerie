e09ab909fc395f086ecee7b3fcadb744fc83e56a
Yivlx <vuxdev@gmail.com>
2017-11-19 13:16:05 -0800

    Int Methods

---
+++ Extensions/IntExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Valerie.Extensions
+{
+    public class IntExt
+    {
+        public static int GiveXp(int Xp) => Xp / (int)Math.Sqrt(DateTime.Now.Millisecond);
+
+        public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 4.0);
+
+        public static int GetXpForNextLevel(int Level) => (int)Math.Pow(Level, 4);
+    }
+}
