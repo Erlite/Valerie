0b00abed6424bfd455b97a928aa7b7048a912706
Yivlx <vuxdev@gmail.com>
2017-11-19 13:12:49 -0800

    Get Random Color

--- Handlers/ModuleHandler/ValerieEmbed.cs
+++ Handlers/ModuleHandler/ValerieEmbed.cs
@@ -1,14 +1,12 @@
-﻿using Discord;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using Discord;
 
 namespace Valerie.Handlers.ModuleHandler
 {
     public class ValerieEmbed
     {
         public static EmbedBuilder Embed(EmbedColor Color, string AuthorIcon = null, string AuthorName = null, string AuthorUrl = null,
-            string Description = null, string FooterIcon = null, string FooterText = null, 
+            string Description = null, string FooterIcon = null, string FooterText = null,
             string ImageUrl = null, string ThumbUrl = null, string Title = null, string Url = null)
         {
             return Embed(Color)
@@ -33,11 +31,14 @@ namespace Valerie.Handlers.ModuleHandler
         static EmbedBuilder Embed(EmbedColor Color)
         {
             var embed = new EmbedBuilder();
+            var Random = new Random();
             switch (Color)
             {
                 case EmbedColor.Yellow: embed.Color = new Color(247, 255, 25); break;
                 case EmbedColor.Pastel: embed.Color = new Color(247, 131, 227); break;
                 case EmbedColor.Red: embed.Color = new Color(232, 27, 78); break;
+                case EmbedColor.Cyan: embed.Color = new Color(26, 221, 160); break;
+                case EmbedColor.Random: embed.Color = new Color(Random.Next(256), Random.Next(256), Random.Next(256)); break;
             }
             return embed;
         }
@@ -48,5 +49,7 @@ namespace Valerie.Handlers.ModuleHandler
         Red,
         Pastel,
         Yellow,
+        Cyan,
+        Random
     }
 }
