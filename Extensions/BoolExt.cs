21129a60d09a6d2a6949286fd17977769237ff9e
Yivlx <vuxdev@gmail.com>
2017-11-19 13:15:52 -0800

    Change Tuple Return Type

--- Extensions/BoolExt.cs
+++ Extensions/BoolExt.cs
@@ -4,11 +4,11 @@ namespace Valerie.Extensions
 {
     public static class BoolExt
     {
-        public static (bool, Uri) IsValidUrl(this string Url)
+        public static (bool, string) IsValidUrl(this string Url)
         {
             bool UriCheck = Uri.TryCreate(Url, UriKind.Absolute, out Uri Result) &&
                 (Result.Scheme == Uri.UriSchemeHttp || Result.Scheme == Uri.UriSchemeHttps);
-            return UriCheck == true && Result != null ? (true, Result) : (false, null);
+            return UriCheck == true && Result != null ? (true, Url) : (false, Url);
         }
     }
 }
