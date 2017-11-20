bc327f0fab9ee1e1bb85390085f563e0aa7d9f41
Yivlx <vuxdev@gmail.com>
2017-11-19 18:01:11 -0800

    Detail Command For More Information On A Command.

--- Modules/HelpModule.cs
+++ Modules/HelpModule.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using Discord.Commands;
 using System.Threading.Tasks;
-using Valerie.Handlers.ModuleHandler;
 using System.Collections.Generic;
+using Valerie.Handlers.ModuleHandler;
 
 namespace Valerie.Modules
 {
@@ -27,6 +27,22 @@ namespace Valerie.Modules
             foreach (var Commands in Module)
                 Embed.AddField(Commands.Key, string.Join(", ", Commands.Select(x => x.Name).Distinct()));
             await ReplyAsync(string.Empty, embed: Embed.Build());
+        }
+
+        [Command("Detail"), Alias("Cmd", "Help"), Summary("Shows more information about a command and it's usage.")]
+        public async Task CommandInfoAsync([Remainder] string CommandName)
+        {
+            var Search = CommandService.Search(Context, CommandName);
+            string Description = null;
+            foreach (var MatchedCommand in Search.Commands)
+            {
+                var Command = MatchedCommand.Command;
+                string Aliases = !Command.Aliases.Any() ? "Command has no aliases." : string.Join(", ", Command.Aliases);
+                string Parameters = !Command.Parameters.Any() ? "Command has no parameters." : string.Join(", ", Command.Parameters.Select(x => x.Name));
+                Description += $"**{Aliases}** | **Arguments:** {Parameters}\n**Summary:** {Command.Summary}\n" +
+                    $"**Usage: ** {Context.Config.Prefix}{Command.Name} {string.Join(" ", Command.Parameters.Select(x => $"`<{x.Name}>`"))}\n\n";
+            }
+            await ReplyAsync(Description);
         }
 
         async Task<IEnumerable<CommandInfo>> CheckPermsAsync()
