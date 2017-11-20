08636e462cf984fab5b8c9151f9f4a34cb3bfca3
Yivlx <vuxdev@gmail.com>
2017-11-19 18:00:02 -0800

    List Check Bool Ext.

--- Extensions/BoolExt.cs
+++ Extensions/BoolExt.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Valerie.Extensions
 {
     public static class BoolExt
     {
-        public static (bool, string) IsValidUrl(this string Url)
+        public static (bool, string) IsValidUrl(string Url)
         {
             bool UriCheck = Uri.TryCreate(Url, UriKind.Absolute, out Uri Result) &&
                 (Result.Scheme == Uri.UriSchemeHttp || Result.Scheme == Uri.UriSchemeHttps);
             return UriCheck == true && Result != null ? (true, Url) : (false, Url);
         }
+
+        public static (bool, string) IsMatch(List<string> List, string Msg, string Warning)
+            => List.Any(x => Msg.Contains(x)) ? (true, Warning) : (false, Warning);
+
     }
 }
