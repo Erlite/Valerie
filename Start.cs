18048c7a25e55d66de70eab9a13c47620d55943c
Yivlx <vuxdev@gmail.com>
2017-11-25 13:27:47 -0800

    Inject More Services

--- Start.cs
+++ Start.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Discord;
+using Valerie.Services;
 using Valerie.Handlers;
 using System.Net.Http;
 using Discord.Commands;
 using Discord.WebSocket;
 using Raven.Client.Documents;
 using System.Threading.Tasks;
+using Raven.Client.Documents.Session;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Valerie
@@ -28,17 +30,23 @@ namespace Valerie
                     CaseSensitiveCommands = false,
                     DefaultRunMode = RunMode.Async
                 }))
-                .AddSingleton(new DocumentStore
+                .AddSingleton<IDocumentStore>(new DocumentStore
                 {
-                    Database = "Valerie",
-                    Urls = new[] { "http://localhost:8000" }
+                    Database = "Val",
+                    Urls = new[] { "http://127.0.0.1:8000" }
                 }.Initialize())
+                .AddSingleton<HttpClient>()
                 .AddSingleton<MainHandler>()
+                .AddSingleton<NsfwService>()
                 .AddSingleton<ConfigHandler>()
                 .AddSingleton<EventsHandler>()
                 .AddSingleton<ServerHandler>()
-                .AddSingleton(new HttpClient())
-                .AddSingleton(new Random(Guid.NewGuid().GetHashCode()));
+                .AddSingleton(new Random(Guid.NewGuid().GetHashCode()))
+                .AddTransient<IDocumentSession>(x =>
+                {
+                    using (var Session = x.GetRequiredService<IDocumentStore>().OpenSession())
+                        return Session;
+                });
 
             var Provider = Services.BuildServiceProvider();
             await Provider.GetRequiredService<MainHandler>().StartAsync();
