994ab1603e58809a6bb4ffca029a51f62352c819
Yivlx <vuxdev@gmail.com>
2017-11-25 16:43:20 -0800

    Forgot this one.

--- Handlers/ConfigHandler.cs
+++ Handlers/ConfigHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Valerie.Services;
 using Valerie.JsonModels;
-using Raven.Client.Documents.Session;
 using Raven.Client.Documents;
 
 namespace Valerie.Handlers
@@ -9,40 +8,49 @@ namespace Valerie.Handlers
     public class ConfigHandler
     {
         IDocumentStore Store { get; }
-        IDocumentSession Session { get; }
 
-        public ConfigHandler(IDocumentStore GetStore, IDocumentSession GetSession)
+        public ConfigHandler(IDocumentStore GetStore)
         {
             Store = GetStore;
-            Session = GetSession;
         }
 
         public ConfigModel Config
         {
-            get => Session.Load<ConfigModel>("Config");
+            get
+            {
+                using (var Session = Store.OpenSession())
+                    return Session.Load<ConfigModel>("Config");
+            }
         }
 
         public void LoadConfig()
         {
-            if (Session.Advanced.Exists("Config")) return;
-            LogClient.Write(Source.CONFIG, "Enter Token: ");
-            string Token = Console.ReadLine();
-            LogClient.Write(Source.CONFIG, "Enter Prefix: ");
-            string Prefix = Console.ReadLine();
-            Session.Store(new ConfigModel
+            using (var Session = Store.OpenSession())
             {
-                Id = "Config",
-                Token = Token,
-                Prefix = Prefix
-            });
-            Session.SaveChanges();
+                if (Session.Advanced.Exists("Config")) return;
+                LogClient.Write(Source.CONFIG, "Enter Token: ");
+                string Token = Console.ReadLine();
+                LogClient.Write(Source.CONFIG, "Enter Prefix: ");
+                string Prefix = Console.ReadLine();
+                Session.Store(new ConfigModel
+                {
+                    Id = "Config",
+                    Token = Token,
+                    Prefix = Prefix
+                });
+                Session.SaveChanges();
+            }
         }
 
         public void Save(ConfigModel GetConfig = null)
         {
             GetConfig = GetConfig ?? Config;
-            Session.Store(GetConfig, "Config");
-            Session.SaveChanges();
+            if (GetConfig == null) return;
+            using (var Session = Store.OpenSession())
+            {
+                Session.Store(GetConfig, "Config");
+                Session.SaveChanges();
+            }
         }
     }
 }
