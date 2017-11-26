36daeaff1bfc4a6ed2fe07dfd347e98e60cfec8f
Yivlx <vuxdev@gmail.com>
2017-11-26 13:33:50 -0800

    Fixed NSFW Commands

--- Modules/NSFWModule.cs
+++ Modules/NSFWModule.cs
@@ -1,35 +1,28 @@
 ﻿using System;
+using Discord;
 using System.Linq;
 using Newtonsoft.Json;
-using Valerie.Attributes;
 using Discord.Commands;
+using Valerie.Attributes;
 using Valerie.JsonModels;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Valerie.Handlers.ModuleHandler;
-using AngleSharp;
-using AngleSharp.Dom.Html;
-using AngleSharp.Parser.Html;
-using Valerie.Services;
+using System.Text.RegularExpressions;
 
 namespace Valerie.Modules
 {
-    [Name("NSFW Commands"), @RequireNsfw]
-    public class NSFWModule : ValerieBase
+    [Name("NSFW Commands"), RequireBotPermission(ChannelPermission.SendMessages), @RequireNsfw]
+    public class NsfwModule : ValerieBase
     {
-        NsfwService NsfwService { get; }
-        public NSFWModule(NsfwService GetService)
-        {
-            NsfwService = GetService;
-        }
-
         [Command("Boobs"), Summary("Oh my, you naughty lilttle boiii!")]
         public async Task BoobsAsync()
-            => await ReplyAsync(await NsfwService.RuNsfwAsync("http://api.oboobs.ru/boobs/", 11272));
+            => await ReplyAsync(await RuNsfwAsync("http://api.oboobs.ru/boobs/", 11272));
 
         [Command("Ass"), Summary("I can't believe you need help with this command.")]
         public async Task BumsAsync()
-            => await ReplyAsync(await NsfwService.RuNsfwAsync("http://api.obutts.ru/butts/", 5265));
+            => await ReplyAsync(await RuNsfwAsync("http://api.obutts.ru/butts/", 5265));
 
         [Command("Porn"), Summary("Uses Porn.com API to fetch videos.")]
         public async Task PornAsync([Remainder] string Search)
@@ -55,6 +48,64 @@ namespace Valerie.Modules
                 await ReplyAsync(Token["neko"].ToString());
             }
             catch { }
+        }
+
+        async Task<string> RuNsfwAsync(string Url, int Max)
+        {
+            try
+            {
+                var Parse = JArray.Parse(await Context.HttpClient.GetStringAsync($"{Url}{Context.Random.Next(Max)}").ConfigureAwait(false))[0];
+                return ($"{Url}{Parse["preview"]}");
+            }
+            catch { return null; }
+        }
+
+        async Task<string> WeebAsync(Weeb Weeb, string Link, List<string> Tags)
+        {
+            string Url = null;
+            string Result = null;
+            MatchCollection Matches = null;
+
+            switch (Weeb)
+            {
+                case Weeb.Danbooru: Url = $"http://danbooru.donmai.us/posts?page={Context.Random.Next(0, 15)}{string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+                case Weeb.Gelbooru: Url = $"http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags={string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+                case Weeb.Rule34: Url = $"http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+                case Weeb.Cureninja: Url = $"https://cure.ninja/booru/api/json?f=a&o=r&s=1&q={string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+                case Weeb.Konachan: Url = $"http://konachan.com/post?page={Context.Random.Next(0, 5)}&tags={string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+                case Weeb.Yandere: Url = $"https://yande.re/post.xml?limit=25&page={Context.Random.Next(0, 15)}&tags={string.Join("+", Tags.Select(x => x.Replace(" ", "_")))}"; break;
+            }
+            var Get = await Context.HttpClient.GetStringAsync(Url).ConfigureAwait(false);
+            switch (Weeb)
+            {
+                case Weeb.Danbooru: Matches = Regex.Matches(Get, "data-large-file-url=\"(.*)\""); break;
+                case Weeb.Yandere:
+                case Weeb.Gelbooru:
+                case Weeb.Rule34: Matches = Regex.Matches(Get, "file_url=\"(.*?)\" "); break;
+                case Weeb.Cureninja: Matches = Regex.Matches(Get, "\"url\":\"(.*?)\""); break;
+                case Weeb.Konachan: Matches = Regex.Matches(Get, "<a class=\"directlink smallimg\" href=\"(.*?)\""); break;
+            }
+            if (!Matches.Any()) return "No results found.";
+            switch (Weeb)
+            {
+                case Weeb.Danbooru: Result = $"http://danbooru.donmai.us/{Matches[Context.Random.Next(Matches.Count)].Groups[1].Value}"; break;
+                case Weeb.Konachan:
+                case Weeb.Gelbooru: Result = $"http:{Matches[Context.Random.Next(Matches.Count)].Groups[1].Value}"; break;
+                case Weeb.Yandere:
+                case Weeb.Rule34: Result = Matches[Context.Random.Next(Matches.Count)].Groups[1].Value; break;
+                case Weeb.Cureninja: Result = Matches[Context.Random.Next(Matches.Count)].Groups[1].Value.Replace("\\/", "/"); break;
+            }
+            return Result;
+        }
+
+        public enum Weeb
+        {
+            Rule34,
+            Yandere,
+            Gelbooru,
+            Danbooru,
+            Cureninja,
+            Konachan
         }
     }
 }
