6c51e6f4a3777ef2f95af168001756b834202a2e
Yivlx <vuxdev@gmail.com>
2017-11-29 17:25:53 -0800

    Removed GiveXP method

--- Extensions/IntExt.cs
+++ Extensions/IntExt.cs
@@ -5,8 +5,6 @@ namespace Valerie.Extensions
 {
     public class IntExt
     {
-        public static int GiveXp(int Xp) => Xp / (int)Math.Sqrt(DateTime.Now.Millisecond);
-
         public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 4.0);
 
         public static int GetXpForNextLevel(int Level) => (int)Math.Pow(Level, 4);
