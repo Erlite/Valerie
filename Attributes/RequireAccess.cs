3fb1449b7a0fce667ddaae28471b7a69d40643fd
Yivlx <vuxdev@gmail.com>
2017-12-03 14:53:59 -0800

    Added BotOwner Permission

--- Attributes/RequireAccess.cs
+++ Attributes/RequireAccess.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using System.Threading.Tasks;
 using Valerie.Handlers.ModuleHandler;
 
 namespace Valerie.Attributes
@@ -15,16 +15,13 @@ namespace Valerie.Attributes
         {
             var Context = (context as IContext);
             var User = Context.User as IGuildUser;
-            var AdminPerms = (User.Id == Context.Guild.OwnerId || User.GuildPermissions.Administrator || Context.Server.Admins.Contains(User.Id));
+            var AdminPerms = (User.Id == Context.Guild.OwnerId || User.GuildPermissions.Administrator || Context.Server.Admins.Contains(User.Id) ||
+                User.Id == Context.Client.GetApplicationInfoAsync().GetAwaiter().GetResult().Owner.Id);
             var ModPerms = (User.GuildPermissions.KickMembers || User.GuildPermissions.BanMembers || User.GuildPermissions.ManageRoles || User.GuildPermissions.ManageMessages);
-            if (GetAccessLevel == AccessLevel.Admins && AdminPerms)
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            else if (GetAccessLevel == AccessLevel.Mods && ModPerms)
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            else if (GetAccessLevel == AccessLevel.AdminsNMods && (AdminPerms || ModPerms))
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            else
-                return Task.FromResult(PreconditionResult.FromError($"{Command.Name} requires {typeof(AccessLevel)} access."));
+            if (GetAccessLevel == AccessLevel.Admins && AdminPerms) return Task.FromResult(PreconditionResult.FromSuccess());
+            else if (GetAccessLevel == AccessLevel.Mods && ModPerms) return Task.FromResult(PreconditionResult.FromSuccess());
+            else if (GetAccessLevel == AccessLevel.AdminsNMods && (AdminPerms || ModPerms)) return Task.FromResult(PreconditionResult.FromSuccess());
+            else return Task.FromResult(PreconditionResult.FromError($"{Command.Name} is limited to {GetAccessLevel}."));
         }
     }
 
