4acee3be72d66b29199b435718b679236ace0786
Yivlx <vuxdev@gmail.com>
2017-12-09 19:15:34 -0800

    DateTime Extension

---
+++ Extensions/DateExt.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Valerie.Extensions
+{
+    public class DateExt
+    {
+        public static DateTime UnixDT(double Unix) => new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(Unix).ToLocalTime();
+    }
+}
