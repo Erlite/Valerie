480c936a56e4e65d0259c4ccef9cf909a999a56f
Yivlx <vuxdev@gmail.com>
2018-01-23 05:43:08 -0800

    Changed Method Signature and Cleaned up namespaces

--- Handlers/ServerHandler.cs
+++ Handlers/ServerHandler.cs
@@ -1,8 +1,8 @@
-﻿using Valerie.Services;
+﻿using System;
+using System.Linq;
+using Valerie.Services;
 using Valerie.JsonModels;
 using Raven.Client.Documents;
-using System.Linq;
-using System;
 
 namespace Valerie.Handlers
 {
@@ -53,7 +53,7 @@ namespace Valerie.Handlers
             }
         }
 
-        public void MemoryUpdate(ulong GuildId, ulong UserId, float Bytes)
+        public void MemoryUpdate(ulong GuildId, ulong UserId, int Bytes)
         {
             var Server = GetServer(GuildId);
             var MemUser = Server.Memory.FirstOrDefault(x => x.Id == $"{UserId}");
