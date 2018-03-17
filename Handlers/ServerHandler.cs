63f7769852958675e2fe39f3ea4c6a8dcfa52e39
Yivlx <vuxdev@gmail.com>
2018-03-17 09:03:59 -0700

    Bug Fixes

--- Handlers/ServerHandler.cs
+++ Handlers/ServerHandler.cs
@@ -1,8 +1,7 @@
-﻿using Valerie.Services;
+﻿using System;
+using Valerie.Services;
 using Valerie.JsonModels;
 using Raven.Client.Documents;
-using System.Linq;
-using System;
 
 namespace Valerie.Handlers
 {
@@ -21,11 +20,11 @@ namespace Valerie.Handlers
                 return Session.Load<ServerModel>($"{Id}");
         }
 
-        public void RemoveServer(ulong Id)
+        public void RemoveServer(ulong Id, string Name = null)
         {
-            using (var Session = Store.OpenSession())
-                Session.Delete($"{Id}");
-            LogClient.Write(Source.SERVER, $"Removed Server With Id: {Id}");
+            using (var Session = Store.OpenSession()) Session.Delete($"{Id}");
+            string Msg = string.IsNullOrWhiteSpace(Name) ? $"Removed Server With Id: {Id}" : $"Removed Config For {Name}";
+            LogClient.Write(Source.SERVER, Msg);
         }
 
         public void AddServer(ulong Id, string Name = null)
@@ -57,13 +56,12 @@ namespace Valerie.Handlers
         public void MemoryUpdate(ulong GuildId, ulong UserId, int Bytes)
         {
             var Server = GetServer(GuildId);
-            if (!Server.Profiles.ContainsKey(UserId))
-                Server.Profiles.Add(UserId, new UserProfile
-                {
-                    Bytes = Bytes,
-                    DailyStreak = 0,
-                    DailyReward = DateTime.Now
-                });
+            if (!Server.Profiles.ContainsKey(UserId)) Server.Profiles.Add(UserId, new UserProfile
+            {
+                Bytes = Bytes,
+                DailyStreak = 0,
+                DailyReward = DateTime.Now
+            });
             else
             {
                 var MemUser = Server.Profiles[UserId];
