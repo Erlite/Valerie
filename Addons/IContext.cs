9a6c6364595743379a1d957df5e101efe53e0538
Yivlx <vuxdev@gmail.com>
2018-03-25 20:15:14 -0700

    Litttt Fam

--- Addons/IContext.cs
+++ Addons/IContext.cs
@@ -1,46 +1,45 @@
 ﻿using System;
 using Discord;
-using Valerie.Services;
+using Valerie.Models;
+using Valerie.Handlers;
 using System.Net.Http;
 using Discord.Commands;
-using Valerie.JsonModels;
+using Discord.WebSocket;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Valerie.Handlers.ModuleHandler
+namespace Valerie.Addons
 {
     public class IContext : ICommandContext
     {
         public IUser User { get; }
         public IGuild Guild { get; }
         public Random Random { get; }
-        public ConfigModel Config { get; }
-        public ServerModel Server { get; }
+        public GuildModel Server { get; }
+        public ConfigModel Config { get; }        
         public IDiscordClient Client { get; }
         public HttpClient HttpClient { get; }
         public IUserMessage Message { get; }
         public IMessageChannel Channel { get; }
+        public GuildHandler GuildHandler { get; }
         public IDocumentSession Session { get; }
         public ConfigHandler ConfigHandler { get; }
-        public RedditService RedditService { get; }
-        public ServerHandler ServerHandler { get; }
 
         public IContext(IDiscordClient ClientParam, IUserMessage MessageParam, IServiceProvider ServiceProvider)
         {
-            Client = ClientParam;
-            Message = MessageParam;
-            User = MessageParam.Author;
-            Channel = MessageParam.Channel;
-            Guild = (MessageParam.Channel as IGuildChannel).Guild;
+            Client = ClientParam as DiscordSocketClient;
+            User = MessageParam.Author as SocketUser;            
+            Message = MessageParam as SocketUserMessage;            
+            Channel = MessageParam.Channel as SocketTextChannel;
+            Guild = (MessageParam.Channel as IGuildChannel).Guild as SocketGuild;
             Random = ServiceProvider.GetRequiredService<Random>();
             HttpClient = ServiceProvider.GetRequiredService<HttpClient>();
+            GuildHandler = ServiceProvider.GetRequiredService<GuildHandler>();
             Config = ServiceProvider.GetRequiredService<ConfigHandler>().Config;
             ConfigHandler = ServiceProvider.GetRequiredService<ConfigHandler>();
-            RedditService = ServiceProvider.GetRequiredService<RedditService>();
-            ServerHandler = ServiceProvider.GetRequiredService<ServerHandler>();
             Session = ServiceProvider.GetRequiredService<IDocumentStore>().OpenSession();
-            Server = ServiceProvider.GetRequiredService<ServerHandler>().GetServer(Guild.Id);
+            Server = ServiceProvider.GetRequiredService<GuildHandler>().GetServer(Guild.Id);
         }
     }
 }
