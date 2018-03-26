56d32e08c91feffe6c16138a02f817732c4155e6
Yivlx <vuxdev@gmail.com>
2018-03-25 18:45:49 -0700

    Predefined Values.

---
+++ Models/ConfigModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Valerie.Models
+{
+    public class ConfigModel
+    {
+        public string Id { get; set; }
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+        public string ReportChannel { get; set; }
+        public string ServerMessage { get; set; }
+        public List<string> Games { get; set; } = new List<string>();
+        public List<string> Imports { get; set; } = new List<string>()
+        {
+            "System", "System.IO", "System.Reflection", "System.Linq", "System.Collections.Generic",
+            "System.Net.Http", "System.Threading", "Newtonsoft.Json"
+        };
+        public Dictionary<ulong, string> Blacklist { get; set; } = new Dictionary<ulong, string>();
+        public Dictionary<string, string> APIKeys { get; set; } = new Dictionary<string, string>()
+        { {"Giphy", "dc6zaTOxFJmzC" }, {"Google", "" }, {"Steam", "" }, {"Imgur", "" }, {"Cleverbot", "" } };
+    }
+}
