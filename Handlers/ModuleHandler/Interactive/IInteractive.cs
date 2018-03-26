9b74ecefc044333f816a2e085784270e3ac4cf7d
Yivlx <vuxdev@gmail.com>
2018-03-25 18:45:03 -0700

    Begin 2018 Rewrite

--- Handlers/ModuleHandler/Interactive/IInteractive.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Valerie.Handlers.ModuleHandler.Interactive
-{
-    public interface IInteractive<T>
-    {
-        Task<bool> JudgeAsync(IContext Context, T TypeParameter);
-    }
-}
