dd6fc812be2e051b21e7e7aa974218b0ff3d00fe
Yivlx <vuxdev@gmail.com>
2018-03-27 18:57:50 -0700

    RequireNSFW is back

---
+++ Preconditions/RequireNSFW.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Valerie.Preconditions
+{
+    public class RequireNSFW : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext Context, CommandInfo Info, IServiceProvider Provider)
+        {
+            var Channel = Context.Channel as Discord.ITextChannel;
+            if (Channel.IsNsfw || Channel.Name.Contains("nsfw")) return Task.FromResult(PreconditionResult.FromSuccess());
+            return Task.FromResult(PreconditionResult.FromError($"**{Info.Name}** command can only be ran in NSFW channel, pervert."));
+        }
+    }
+}
