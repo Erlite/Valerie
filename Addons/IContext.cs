3eb359f88b1bb943ca3270fe82310d8700a82f52
Yivlx <vuxdev@gmail.com>
2018-03-27 18:47:40 -0700

    Woops cleanup

--- Addons/IContext.cs
+++ Addons/IContext.cs
@@ -4,7 +4,6 @@ using Valerie.Models;
 using Valerie.Handlers;
 using System.Net.Http;
 using Discord.Commands;
-using Discord.WebSocket;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,18 +27,18 @@ namespace Valerie.Addons
 
         public IContext(IDiscordClient ClientParam, IUserMessage MessageParam, IServiceProvider ServiceProvider)
         {
-            Client = ClientParam as DiscordSocketClient;
-            User = MessageParam.Author as SocketUser;            
-            Message = MessageParam as SocketUserMessage;            
-            Channel = MessageParam.Channel as SocketTextChannel;
-            Guild = (MessageParam.Channel as IGuildChannel).Guild as SocketGuild;
+            Client = ClientParam;
+            Message = MessageParam;
+            User = MessageParam.Author;            
+            Channel = MessageParam.Channel;
+            Guild = (MessageParam.Channel as IGuildChannel).Guild;
             Random = ServiceProvider.GetRequiredService<Random>();
             HttpClient = ServiceProvider.GetRequiredService<HttpClient>();
             GuildHandler = ServiceProvider.GetRequiredService<GuildHandler>();
             Config = ServiceProvider.GetRequiredService<ConfigHandler>().Config;
             ConfigHandler = ServiceProvider.GetRequiredService<ConfigHandler>();
             Session = ServiceProvider.GetRequiredService<IDocumentStore>().OpenSession();
-            Server = ServiceProvider.GetRequiredService<GuildHandler>().GetServer(Guild.Id);
+            Server = ServiceProvider.GetRequiredService<GuildHandler>().GetGuild(Guild.Id);
         }
     }
 }
