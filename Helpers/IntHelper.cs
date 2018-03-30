18459d3f21faa6d8d95aa0bbcc618e22446fa58a
Yivlx <vuxdev@gmail.com>
2018-03-30 14:33:13 -0700

    Helper methods for DataTypes

---
+++ Helpers/IntHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Valerie.Helpers
+{
+    public class IntHelper
+    {
+        public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 3.5);
+
+        public static int GetXpForNextLevel(int Level) => (int)Math.Pow(Level, 3.5);
+    }
+}
---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Discord.WebSocket;
+
+namespace Valerie.Helpers
+{
+    public class StringHelper
+    {
+        public static string CheckUser(DiscordSocketClient Client, ulong UserId)
+        {
+            var User = Client.GetUser(UserId);
+            return User == null ? "Unknown User." : User.Username;
+        }
+
+        public static string CheckRole(SocketGuild Guild, ulong Id)
+        {
+            var Role = Guild.GetRole(Id);
+            return Role == null ? "Unknown Role." : Role.Name;
+        }
+
+        public static string Star(int Stars)
+        {
+            if (Stars <= 5 && Stars > 0) return "⭐";
+            else if (Stars > 5) return "🌟";
+            else if (Stars > 15) return "💫";
+            else return "✨";
+        }
+
+        public static string Replace(string Message, string Guild = null, string User = null, int Level = 0, int Crystals = 0)
+        {
+            StringBuilder Builder = new StringBuilder(Message);
+            Builder.Replace("{guild}", Guild);
+            Builder.Replace("{user}", User);
+            Builder.Replace("{level}", $"{Level}");
+            Builder.Replace("{crystals}", $"{Crystals}");
+            return Builder.ToString();
+        }
+    }
+}
