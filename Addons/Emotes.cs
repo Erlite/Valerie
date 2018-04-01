aad6bdea45ea7dd3b8bf5ef9547027af360bc492
Yivlx <vuxdev@gmail.com>
2018-04-01 06:36:20 -0700

    Emotes from other guilds

---
+++ Addons/Emotes.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+
+namespace Valerie.Addons
+{
+    public class Emotes
+    {
+        public static Emote DHeart { get => Emote.Parse("<:hrt:419635124339212308>"); }
+        public static Emote DWink { get => Emote.Parse("<:dblWink:376811927994761216>"); }
+        public static Emote DEyes { get => Emote.Parse("<:zoomeyes:390046883281633290>"); }
+        public static Emote DHammer { get => Emote.Parse("<:dblMod:401724520806875139>"); }
+        public static Emote DSupporter { get => Emote.Parse("<:dblSupporter:401725034453925889>"); }
+    }
+}
---
+++ Modules/GeneralModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using System.IO;
+using Valerie.Addons;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using static Valerie.Addons.Embeds;
+
+namespace Valerie.Modules
+{
+    [Name("General Commands"), RequireBotPermission(ChannelPermission.SendMessages)]
+    public class GeneralModule : Base
+    {
+        [Command("Ping"), Summary("Replies back with a pong?")]
+        public Task PingAsync() => ReplyAsync(string.Empty, BuildEmbed(Paint.Lime)
+            .AddField("Gateway Latency", $"{(Context.Client as DiscordSocketClient).Latency}ms").Build());
+    }
+}
