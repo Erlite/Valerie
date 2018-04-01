5498646e2eeec9e147548bddf99222910f9d99ee
Yivlx <vuxdev@gmail.com>
2018-04-01 06:38:15 -0700

    GithubModel for stats command

---
+++ Models/GithubModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Valerie.Models
+{
+    public partial class GithubModel
+    {
+        [JsonProperty("commit")]
+        public GithubModel Commit { get; set; }
+
+        [JsonProperty("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+    }
+
+    public partial class GithubModel
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
