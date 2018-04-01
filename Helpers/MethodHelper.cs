f8406162d1090a652582bd1293f9f3200d5ffc4b
Yivlx <vuxdev@gmail.com>
2018-04-01 06:37:33 -0700

    Generic Methods

---
+++ Helpers/MethodHelper.cs
@@ -0,0 +1,23 @@
+﻿using Valerie.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Valerie.Helpers
+{
+    public class MethodHelper
+    {
+        HttpClient HttpClient { get; }
+        public MethodHelper(HttpClient httpClient) => HttpClient = httpClient;
+
+        public async Task<IReadOnlyCollection<GithubModel>> GetCommitsAsync()
+        {
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+            var Request = await HttpClient.GetAsync("https://api.github.com/repos/Yucked/Valerie/commits");
+            var Content = JsonConvert.DeserializeObject<IReadOnlyCollection<GithubModel>>(await Request.Content.ReadAsStringAsync());
+            HttpClient.DefaultRequestHeaders.Clear();
+            return Content;
+        }
+    }
+}
