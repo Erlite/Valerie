6fa1a82ba5a0f2e14dc913b37ff40b4f90bce132
Yivlx <vuxdev@gmail.com>
2018-04-01 18:45:27 -0700

    Very nice :ok_hand:

--- Modules/OwnerModule.cs
+++ Modules/OwnerModule.cs
@@ -1,5 +1,7 @@
-﻿using Discord;
+﻿using System;
+using Discord;
 using System.IO;
+using Valerie.Enums;
 using Valerie.Addons;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -10,34 +12,36 @@ namespace Valerie.Modules
     [Name("Valerie's Owner Commands"), RequireOwner]
     public class OwnerModule : Base
     {
-        [Command("Update"), Summary("Updates ")]
-        public async Task UpdateAsync(Update Update, [Remainder] string Value)
+        [Command("Update"), Summary("Upd")]
+        public async Task UpdateAsync(UpdateType Update, [Remainder] string Value)
         {
             switch (Update)
             {
-                case Update.Avatar:
+                case UpdateType.Avatar:
                     using (var Picture = new FileStream(Value, FileMode.Open))
                         await Context.Client.CurrentUser.ModifyAsync(x => x.Avatar = new Image(Picture));
                     break;
-                case Update.Username:
+                case UpdateType.Username:
                     await Context.Client.CurrentUser.ModifyAsync(x => x.Username = Value);
                     break;
+                case UpdateType.Status:
+                    var Split = Value.Split('-');
+                    await (Context.Client as DiscordSocketClient).SetActivityAsync(
+                        new Game(Split[1], (ActivityType)Enum.Parse(typeof(ActivityType), Split[0]))).ConfigureAwait(false);
+                    break;
+                case UpdateType.Prefix:
+                    Context.Config.Prefix = Value;
+                    break;
             }
-            await ReplyAsync($"{Update} has been updated {Emotes.DWink}");
+            await ReplyAsync($"{Update} has been updated {Emotes.DWink}", Document: DocumentType.Config);
         }
 
-        [Command("GetInvite"), Summary("Makes an invite to the specified guild.")]
+        [Command("GetInvite"), Summary("Makes an invite for the  specified guild.")]
         public async Task GetInviteAsync(ulong GuildId)
         {
             var channel = await Context.Client.GetChannelAsync((await Context.Client.GetGuildAsync(GuildId)).DefaultChannelId);
             var invite = await (channel as SocketGuildChannel).CreateInviteAsync(null);
             await (await Context.User.GetOrCreateDMChannelAsync()).SendMessageAsync($"Here is your invite link: <{invite.Url}>");
-        }
-
-        public enum Update
-        {
-            Avatar,
-            Username
         }
     }
 }
