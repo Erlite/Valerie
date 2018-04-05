0cfaacfc51b6c7364c3027dd1030dcfed5f72283
Yivlx <vuxdev@gmail.com>
2018-04-05 13:51:49 -0700

    Finished RequirePermission Precondition

--- Preconditions/RequirePermission.cs
+++ Preconditions/RequirePermission.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Discord;
+using System.Linq;
 using Valerie.Enums;
 using Valerie.Addons;
 using Discord.Commands;
+using Discord.WebSocket;
 using System.Threading.Tasks;
 
 namespace Valerie.Preconditions
@@ -14,10 +17,16 @@ namespace Valerie.Preconditions
             AccessLevel = accessLevel;
         }
 
-        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo Info, IServiceProvider Provider)
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo Command, IServiceProvider Provider)
         {
             var Context = context as IContext;
-            //var Admins = Context.Server
+            var GuildUser = Context.User as SocketGuildUser;
+            var AdminPerms = Context.GuildHelper.GetProfile(Context.Guild.Id, Context.User.Id).IsAdmin || Context.Guild.OwnerId == Context.User.Id ||
+                GuildUser.GuildPermissions.Administrator || GuildUser.GuildPermissions.ManageGuild;
+            var ModPerms = new[] { GuildPermission.KickMembers, GuildPermission.BanMembers, GuildPermission.ManageChannels, GuildPermission.ManageMessages, GuildPermission.ManageRoles };
+            if (AccessLevel >= AccessLevel.ADMIN && AdminPerms) return Task.FromResult(PreconditionResult.FromSuccess());
+            else if (AccessLevel >= AccessLevel.MODERATOR && ModPerms.Any(x => GuildUser.GuildPermissions.Has(x))) return Task.FromResult(PreconditionResult.FromSuccess());
+            else return Task.FromResult(PreconditionResult.FromError($"{Command.Name} requires **{AccessLevel}** AccessLevel. To learn more on AccessLevel, use `{Context.Config.Prefix}Info` command."));
         }
     }
 }
