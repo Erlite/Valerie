63b62c0a4aee465b710e4e1977bba6896d8be096
Yivlx <vuxdev@gmail.com>
2018-04-05 16:45:22 -0700

    Added Eval Model

---
+++ Models/EvalModel.cs
@@ -0,0 +1,14 @@
+﻿using Valerie.Addons;
+using Discord.WebSocket;
+
+namespace Valerie.Models
+{
+    public class EvalModel
+    {
+        public IContext Context { get; set; }
+        public SocketGuild Guild { get; set; }
+        public SocketGuildUser User { get; set; }
+        public DiscordSocketClient Client { get; set; }
+        public SocketGuildChannel Channel { get; set; }
+    }
+}
