d085132dfea7c3282cb997c5575491db0714cff9
Yivlx <vuxdev@gmail.com>
2018-04-07 14:52:59 -0700

    Harder to Level Up

--- Helpers/IntHelper.cs
+++ Helpers/IntHelper.cs
@@ -4,8 +4,8 @@ namespace Valerie.Helpers
 {
     public class IntHelper
     {
-        public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 3.5);
+        public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 7);
 
-        public static int GetXpForNextLevel(int Level) => (int)Math.Pow(Level, 3.5);
+        public static int GetXpForNextLevel(int Level) => (int)Math.Pow(Level, 7);
     }
 }
