783876e5abc2d99c3f8ad7aa849aa1e0ef51dea7
Yivlx <vuxdev@gmail.com>
2018-04-07 12:56:44 -0700

    Added More Emotes

--- Addons/Emotes.cs
+++ Addons/Emotes.cs
@@ -5,10 +5,12 @@ namespace Valerie.Addons
     public class Emotes
     {
         public static Emote DHeart { get => Emote.Parse("<:hrt:419635124339212308>"); }
+        public static Emote TickNo { get => Emote.Parse("<:tickNo:315009174163685377>"); }
         public static Emote DWink { get => Emote.Parse("<:dblWink:376811927994761216>"); }
+        public static Emote TickYes { get => Emote.Parse("<:tickYes:315009125694177281>"); }
         public static Emote DEyes { get => Emote.Parse("<:zoomeyes:390046883281633290>"); }
+        public static Emote PepeSad { get => Emote.Parse("<:PepeSad:420351909833211904>"); }
         public static Emote DHammer { get => Emote.Parse("<:dblMod:401724520806875139>"); }
         public static Emote DSupporter { get => Emote.Parse("<:dblSupporter:401725034453925889>"); }
-        public static Emote PepeSad { get => Emote.Parse("<:PepeSad:420351909833211904>"); }
     }
 }
