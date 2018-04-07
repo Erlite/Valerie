b7de694ccf7cf2cc4b2a7b6256fe653667b7974d
Yivlx <vuxdev@gmail.com>
2018-04-07 14:29:30 -0700

    Set Command Enums

---
+++ Enums/SettingType.cs
@@ -0,0 +1,16 @@
+﻿namespace Valerie.Enums
+{
+    public enum SettingType
+    {
+        Prefix,        
+        JoinRole,
+        MuteRole,
+        ModChannel,
+        JoinChannel,
+        LeaveChannel,
+        MaxWarnings,
+        RedditChannel,
+        ChatterChannel,
+        StarboardChannel
+    }
+}
