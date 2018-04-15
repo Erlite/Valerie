2e00d3d61a9ad7f3b212b87129a62c1413eaeb40
Yivlx <vuxdev@gmail.com>
2018-04-15 08:40:56 -0700

    Update Service Starts As Soon As The Bot Starts

--- Core.cs
+++ Core.cs
@@ -13,50 +13,51 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Valerie
 {
-	class Core
-	{
-		static void Main(string[] args) => new Core().InitializeAsync().GetAwaiter().GetResult();
+    class Core
+    {
+        static void Main(string[] args) => new Core().InitializeAsync().GetAwaiter().GetResult();
 
-		async Task InitializeAsync()
-		{
-			var Services = new ServiceCollection()
-				.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
-				{
-					AlwaysDownloadUsers = true,
-					LogLevel = LogSeverity.Error,
+        async Task InitializeAsync()
+        {
+            var Services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                {
+                    AlwaysDownloadUsers = true,
+                    LogLevel = LogSeverity.Error,
 #if !OSCHECK
-					WebSocketProvider = WS4NetProvider.Instance
+                    WebSocketProvider = WS4NetProvider.Instance
 #endif
-				}))
-				.AddSingleton(new CommandService(new CommandServiceConfig
-				{
-					ThrowOnError = true,
-					IgnoreExtraArgs = false,
-					CaseSensitiveCommands = false,
-					DefaultRunMode = RunMode.Async
-				}))
-				.AddSingleton<IDocumentStore>(new DocumentStore
-				{
-					Database = "Valerie",
-					Urls = new[] { "http://127.0.0.1:8080" }
-				}.Initialize())
-				.AddSingleton<LogService>()
-				.AddSingleton<GuildHelper>()
-				.AddSingleton<MainHandler>()
-				.AddSingleton<GuildHandler>()
-				.AddSingleton<ConfigHandler>()
-				.AddSingleton<MethodHelper>()
-				.AddSingleton<EventsHandler>()
-				.AddSingleton<UpdateService>()
-				.AddSingleton(new HttpClient())
-				.AddSingleton(new Random(Guid.NewGuid().GetHashCode()));
+                }))
+                .AddSingleton(new CommandService(new CommandServiceConfig
+                {
+                    ThrowOnError = true,
+                    IgnoreExtraArgs = false,
+                    CaseSensitiveCommands = false,
+                    DefaultRunMode = RunMode.Async
+                }))
+                .AddSingleton<IDocumentStore>(new DocumentStore
+                {
+                    Database = "Valerie",
+                    Urls = new[] { "http://127.0.0.1:8080" }
+                }.Initialize())
+                .AddSingleton<LogService>()
+                .AddSingleton<GuildHelper>()
+                .AddSingleton<MainHandler>()
+                .AddSingleton<GuildHandler>()
+                .AddSingleton<ConfigHandler>()
+                .AddSingleton<MethodHelper>()
+                .AddSingleton<EventsHandler>()
+                .AddSingleton<UpdateService>()
+                .AddSingleton(new HttpClient())
+                .AddSingleton(new Random(Guid.NewGuid().GetHashCode()));
 
-			var Provider = Services.BuildServiceProvider();
-			Provider.GetRequiredService<LogService>().Initialize();
-			await Provider.GetRequiredService<MainHandler>().InitializeAsync();
-			await Provider.GetRequiredService<EventsHandler>().InitializeAsync(Provider);
+            var Provider = Services.BuildServiceProvider();
+            Provider.GetRequiredService<LogService>().Initialize();
+            await Provider.GetRequiredService<MainHandler>().InitializeAsync();
+            await Provider.GetRequiredService<EventsHandler>().InitializeAsync(Provider);
+            Provider.GetRequiredService<UpdateService>().InitializeTimer();
 
-			await Task.Delay(-1);
-		}
-	}
+            await Task.Delay(-1);
+        }
+    }
 }
