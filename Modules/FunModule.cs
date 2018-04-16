6ee57a2aebe51f2802a6a7774dd3e1d4c9c87229
WKED <tajamulrabbani1998@gmail.com>
2018-04-16 11:12:44 -0400

    Added Slotmachine, Flip, Expand, Guess, Leaderboards and changed URL of Cats

--- Modules/FunModule.cs
+++ Modules/FunModule.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using Discord;
+using System.Linq;
 using Valerie.Enums;
 using Valerie.Addons;
+using Valerie.Helpers;
 using Discord.Commands;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using static Valerie.Addons.Embeds;
 
 namespace Valerie.Modules
 {
+    [Name("Fun & Games Commands"), RequireBotPermission(ChannelPermission.SendMessages)]
     public class FunModule : Base
     {
         [Command("Clap"), Summary("Replaces spaces in your message with a clap emoji.")]
@@ -44,10 +48,14 @@ namespace Valerie.Modules
 
         [Command("Cat"), Summary("MEOW! Moew meow, meoooow. Meow Meow?")]
         public async Task CatAsync() =>
-            await ReplyAsync($"{JToken.Parse(await Context.HttpClient.GetStringAsync("http://random.cat/meow").ConfigureAwait(false))["file"]}");
+            await ReplyAsync($"{JToken.Parse(await Context.HttpClient.GetStringAsync("http://aws.random.cat/meow").ConfigureAwait(false))["file"]}");
 
         [Command("Rate"), Summary("Rates something for you out of 10.")]
         public async Task RateAsync([Remainder] string ThingToRate) => await ReplyAsync($":thinking: I would rate '{ThingToRate}' a solid {Context.Random.Next(11)}/10");
+
+        [Command("Expand"), Summary("Converts text to full width.")]
+        public Task ExpandAsync([Remainder] string Text)
+            => ReplyAsync(string.Join("", Text.Select(x => StringHelper.Normal.Contains(x) ? x : ' ').Select(x => StringHelper.FullWidth[StringHelper.Normal.IndexOf(x)])));
 
         [Command("Daily"), Summary("Your daily reward. Claim it everyday for higher streak.")]
         public Task DailyAsync()
@@ -72,5 +80,97 @@ namespace Valerie.Modules
             return ReplyAsync($"You've received your daily reward. {Emotes.DHeart}");
         }
 
+        [Command("Slotmachine"), Summary("Want to earn quick crystals? That's how you earn some.")]
+        public Task SlotMachineAsync(int Bet = 100)
+        {
+            var Slots = new string[] { "☄", "🔥", "👾", "🔆", "👀", "👅", "🍑" };
+            var Profile = Context.GuildHelper.GetProfile(Context.Guild.Id, Context.User.Id);
+            if (Profile.Crystals < Bet) return ReplyAsync($"Awww shoot! You don't have enough crystals {Emotes.PepeSad}");
+            var Embed = GetEmbed(Paint.Lime);
+            var GetSlot = new int[]
+            {
+                Context.Random.Next(0, Slots.Length),
+                Context.Random.Next(0, Slots.Length),
+                Context.Random.Next(0, Slots.Length)
+            };
+            Embed.AddField("Slot 1", Slots[GetSlot[0]], true);
+            Embed.AddField("Slot 2", Slots[GetSlot[1]], true);
+            Embed.AddField("Slot 3", Slots[GetSlot[2]], true);
+
+            int win = 0;
+            if (GetSlot[0] == GetSlot[1] & GetSlot[0] == GetSlot[2]) win = 10;
+            else if (GetSlot[0] == GetSlot[1] || GetSlot[0] == GetSlot[2] || GetSlot[1] == GetSlot[2]) win = 2;
+
+            if (win == 0)
+            {
+                Profile.Crystals -= Bet;
+                Embed.Description = $"*Aww..* it seems you lost **{Bet}** crystals {Emotes.PepeSad}\nYou currently have {Profile.Crystals} crystals.";
+            }
+            else
+            {
+                Profile.Crystals -= Bet;
+                Embed.Description = $"**CONGRATS!** You won **{Bet}** crystals {Emotes.DWink}\nYou currently have {Profile.Crystals} crystals.";
+            }
+            Context.GuildHelper.SaveProfile(Context.Guild.Id, Context.User.Id, Profile);
+            return ReplyAsync(string.Empty, Embed.Build());
+        }
+
+        [Command("Flip"), Summary("Flips a coin! DON'T FORGOT TO BET CRYSTALS!")]
+        public Task FlipAsync(char Side, int Bet = 100)
+        {
+            var Profile = Context.GuildHelper.GetProfile(Context.Guild.Id, Context.User.Id);
+            if (Profile.Crystals < Bet) return ReplyAsync($"Awww shoot! You don't have enough crystals {Emotes.PepeSad}");
+            Side = Char.ToLower(Side);
+            bool Heads = Context.Random.Next(0, 101) < 50 ? true : false;
+            if ((Side == 'h' && Heads) || (Side == 't' && !Heads))
+            {
+                Profile.Crystals += Bet;
+                return ReplyAsync($"{Emotes.DWink} You won {Bet} crystals! Currently have {Profile.Crystals} crystals.", Document: DocumentType.Server);
+            }
+            else if ((Side == 'h' && !Heads) || (Side == 't' && Heads))
+            {
+                Profile.Crystals -= Bet;
+                return ReplyAsync($"{Emotes.PepeSad} You lost {Bet} crystals! Currently have {Profile.Crystals} crystals.", Document: DocumentType.Server);
+            }
+            else return ReplyAsync($"Side can either be `h` or `t`.");
+        }
+
+        [Command("Guess"), Summary("Guess the right number to win bytes.")]
+        public async Task GuessAsync()
+        {
+            int MinNum = Context.Random.Next(0, 50);
+            int MaxNum = Context.Random.Next(50, 101);
+            int RandomNum = Context.Random.Next(MinNum, MaxNum);
+            await ReplyAsync($"Guess a number between {MinNum} and {MaxNum}. You have 10 seconds. GO!!!");
+            var UserGuess = await ResponseWaitAsync(Timeout: TimeSpan.FromSeconds(10));
+            if (RandomNum != int.Parse(UserGuess.Content))
+            {
+                await ReplyAsync($"Aww, it seems you guessed the wrong number. The lucky number was: {RandomNum}.");
+                return;
+            }
+            var Profile = Context.GuildHelper.GetProfile(Context.Guild.Id, Context.User.Id);
+            Profile.Crystals += (int)Math.Pow(RandomNum, 2);
+            Context.GuildHelper.SaveProfile(Context.Guild.Id, Context.User.Id, Profile);
+            await ReplyAsync($"Ayyy, you guessed it right {Emotes.DWink}. Got {(int)Math.Pow(RandomNum, 2)} crystals.");
+        }
+
+        [Command("Leaderboards"), Summary("Shows top 10 users with the highest XP for this server.")]
+        public Task LeaderboardAsync()
+        {
+            if (!Context.Server.Profiles.Any()) return ReplyAsync($"{Context.Guild} leadboards is empty.");
+            var Embed = GetEmbed(Paint.Yellow)
+                .WithTitle($"XP Leaderboards For {Context.Guild}");
+
+            var Ordered = Context.Server.Profiles.OrderByDescending(x => x.Value.ChatXP).Where(y => y.Value.ChatXP != 0).Take(10).ToList();
+            if (Ordered.Count > 3)
+            {
+                Embed.AddField($"🥇: {StringHelper.CheckUser(Context.Client, Ordered[0].Key)}", $"**Total XP:** {Ordered[0].Value.ChatXP}", true);
+                Embed.AddField($"🥈: {StringHelper.CheckUser(Context.Client, Ordered[1].Key)}", $"**Total XP:** {Ordered[1].Value.ChatXP}", true);
+                Embed.AddField($"🥉: {StringHelper.CheckUser(Context.Client, Ordered[2].Key)}", $"**Total XP:** {Ordered[2].Value.ChatXP}", true);
+                foreach (var Rank in Ordered.Skip(3)) Embed.AddField($"{StringHelper.CheckUser(Context.Client, Rank.Key)}", $"**Total XP:** {Rank.Value.ChatXP}", true);
+            }
+            else foreach (var Rank in Ordered) Embed.AddField($"{StringHelper.CheckUser(Context.Client, Rank.Key)}", $"**Total XP:** {Rank.Value.ChatXP}", true);
+            return ReplyAsync(string.Empty, Embed.Build());
+        }
     }
 }
