cde2571d3d5085e9c2c257ae9ea45db7ef9c690b
Yivlx <vuxdev@gmail.com>
2018-04-16 16:02:21 -0700

    Guild Rank Method

--- Helpers/IntHelper.cs
+++ Helpers/IntHelper.cs
@@ -1,18 +1,18 @@
 ﻿using System;
+using System.Linq;
+using Valerie.Addons;
 
 namespace Valerie.Helpers
 {
     public class IntHelper
     {
-        static Random Random { get; set; }
-        public IntHelper(Random random) => Random = random;
+        public static int GetLevel(int Xp) => 1 + (int)Math.Pow(Xp, 1 / 4.0);
+        public static int NextLevelXp(int Level) => (int)Math.Pow(Level, 4);
 
-        public static int GiveXP => Random.Next(27) * DateTime.Now.Second /5 > 5 ? (int)Math.Sqrt(DateTime.Now.Second) : DateTime.Now.Second;
-
-        public static int GetLevel(int XP) => 1 + (int)Math.Floor(Math.Pow(XP, 1 / 3.0));
-
-        public static int LastLevelXP(int Level) => (Level - 1) * (Level - 1) * (Level - 1);
-
-        public static int NextLevelXP(int Level) => (int)Math.Pow(Level, 3);
+        public static int GetGuildRank(IContext Context, ulong UserId)
+        {
+            var Get = Context.Server.Profiles.OrderByDescending(x => x.Value.ChatXP).FirstOrDefault(x => x.Key == UserId);
+            return Context.Server.Profiles.ToList().IndexOf(Get);
+        }
     }
 }
