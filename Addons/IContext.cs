7d308aff317f45cdfbf5ec3d2976ed31099af199
WKED <tajamulrabbani1998@gmail.com>
2018-04-18 12:41:32 -0400

    Add RedditService to IContext

--- Addons/IContext.cs
+++ Addons/IContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Discord;
 using Valerie.Models;
-using Valerie.Helpers;
-using Valerie.Handlers;
 using System.Net.Http;
+using Valerie.Helpers;
+using Valerie.Services;
+using Valerie.Handlers;
 using Discord.Commands;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
@@ -17,22 +18,23 @@ namespace Valerie.Addons
         public IGuild Guild { get; }
         public Random Random { get; }
         public GuildModel Server { get; }
-        public ConfigModel Config { get; }        
+        public ConfigModel Config { get; }
+        public IUserMessage Message { get; }
         public IDiscordClient Client { get; }
         public HttpClient HttpClient { get; }
-        public IUserMessage Message { get; }
         public GuildHelper GuildHelper { get; }
         public IMessageChannel Channel { get; }
+        public IDocumentSession Session { get; }        
         public GuildHandler GuildHandler { get; }
-        public IDocumentSession Session { get; }
-        public ConfigHandler ConfigHandler { get; }
         public MethodHelper MethodHelper { get; }
+        public ConfigHandler ConfigHandler { get; }
+        public RedditService RedditService { get; }
 
         public IContext(IDiscordClient ClientParam, IUserMessage MessageParam, IServiceProvider ServiceProvider)
         {
             Client = ClientParam;
             Message = MessageParam;
-            User = MessageParam.Author;            
+            User = MessageParam.Author;
             Channel = MessageParam.Channel;
             Guild = (MessageParam.Channel as IGuildChannel).Guild;
             Random = ServiceProvider.GetRequiredService<Random>();
@@ -42,6 +44,7 @@ namespace Valerie.Addons
             Config = ServiceProvider.GetRequiredService<ConfigHandler>().Config;
             ConfigHandler = ServiceProvider.GetRequiredService<ConfigHandler>();
             MethodHelper = ServiceProvider.GetRequiredService<MethodHelper>();
+            RedditService = ServiceProvider.GetRequiredService<RedditService>();
             Server = ServiceProvider.GetRequiredService<GuildHandler>().GetGuild(Guild.Id);
             Session = ServiceProvider.GetRequiredService<IDocumentStore>().OpenSession();
         }
