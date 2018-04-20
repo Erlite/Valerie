b531a6f4625c66a3fa20f8a6b9b613adc6aceb0f
Yivlx <vuxdev@gmail.com>
2018-04-20 06:00:23 -0700

    Dictionary -> Concurrent

--- Services/RedditService.cs
+++ Services/RedditService.cs
@@ -10,62 +10,52 @@ using Discord.WebSocket;
 using Raven.Client.Documents;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Valerie.Services
 {
     public class RedditService
     {
-        Timer FeedTimer { get; set; }
         HttpClient HttpClient { get; }
+        GuildHandler GuildHandler { get; }
         DiscordSocketClient Client { get; }
-        ConfigHandler ConfigHandler { get; }
-        IDocumentStore DocumentStore { get; }
 
-        Dictionary<ulong, Timer> ChannelTimers { get; set; } = new Dictionary<ulong, Timer>();
-        Dictionary<ulong, List<string>> PostTrack { get; set; } = new Dictionary<ulong, List<string>>();
+        ConcurrentDictionary<ulong, Timer> ChannelTimers { get; set; } = new ConcurrentDictionary<ulong, Timer>();
+        ConcurrentDictionary<ulong, List<string>> PostTrack { get; set; } = new ConcurrentDictionary<ulong, List<string>>();
 
-        public RedditService(HttpClient httpClient, ConfigHandler configHandler, IDocumentStore documentStore, DiscordSocketClient client)
+        public RedditService(HttpClient httpClient, GuildHandler guildHandler, DiscordSocketClient client)
         {
             Client = client;
             HttpClient = httpClient;
-            ConfigHandler = configHandler;
-            DocumentStore = documentStore;
+            GuildHandler = guildHandler;
         }
 
-        public void Initialize() => FeedTimer = new Timer(_ =>
-         {
-             Start();
-         }, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30));
-
-        public void Start()
+        public Task Start(ulong GuildId)
         {
-            List<GuildModel> Configs;
-            using (var Session = DocumentStore.OpenSession())
-                Configs = Session.Query<GuildModel>().Customize(x => x.WaitForNonStaleResults()).ToList();
-            foreach (var Server in Configs.Where(x => x.Reddit.IsEnabled && x.Reddit.TextChannel != 0 && x.Reddit.Subreddits.Any()))
+            var Server = GuildHandler.GetGuild(GuildId);
+            if (!Server.Reddit.IsEnabled || Server.Reddit.TextChannel == 0 || !Server.Reddit.Subreddits.Any()) return Task.CompletedTask;
+            var Channel = Client.GetChannel(Server.Reddit.TextChannel) as IMessageChannel;
+            if (ChannelTimers.ContainsKey(Channel.Id)) return Task.CompletedTask;
+            var ChannelTimer = new Timer(async _ =>
             {
-                var Channel = Client.GetChannel(Server.Reddit.TextChannel) as IMessageChannel;
-                if (ChannelTimers.ContainsKey(Channel.Id)) return;
-                var ChannelTimer = new Timer(async _ =>
+                foreach (var Subbredit in Server.Reddit.Subreddits)
                 {
-                    foreach (var Subbredit in Server.Reddit.Subreddits)
-                    {
-                        var PostIds = new List<string>();
-                        var CheckSub = await SubredditAsync(Subbredit).ConfigureAwait(false);
-                        if (CheckSub == null) return;
-                        var SubData = CheckSub.Data.Children[0].ChildData;
-                        if (PostTrack.ContainsKey(Channel.Id)) PostTrack.TryGetValue(Channel.Id, out PostIds);
-                        if (PostIds.Contains(SubData.Id)) return;
-                        string Description = SubData.Selftext.Length > 500 ? $"{SubData.Selftext.Substring(0, 400)} ..." : SubData.Selftext;
-                        await Channel.SendMessageAsync($"New Post In **r/{SubData.Subreddit}** By **{SubData.Author}**\n" +
-                            $"**{SubData.Title}**\n{Description}\nPost Link: {SubData.Url}").ConfigureAwait(false);
-                        PostIds.Add(SubData.Id);
-                        PostTrack.Remove(Channel.Id);
-                        PostTrack.Add(Channel.Id, PostIds);
-                    }
-                }, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30));
-                ChannelTimers.Add(Channel.Id, ChannelTimer);
-            }
+                    var PostIds = new List<string>();
+                    var CheckSub = await SubredditAsync(Subbredit).ConfigureAwait(false);
+                    if (CheckSub == null) return;
+                    var SubData = CheckSub.Data.Children[0].ChildData;
+                    if (PostTrack.ContainsKey(Channel.Id)) PostTrack.TryGetValue(Channel.Id, out PostIds);
+                    if (PostIds.Contains(SubData.Id)) return;
+                    string Description = SubData.Selftext.Length > 500 ? $"{SubData.Selftext.Substring(0, 400)} ..." : SubData.Selftext;
+                    await Channel.SendMessageAsync($"New Post In **r/{SubData.Subreddit}** By **{SubData.Author}**\n" +
+                        $"**{SubData.Title}**\n{Description}\nPost Link: {SubData.Url}").ConfigureAwait(false);
+                    PostIds.Add(SubData.Id);
+                    PostTrack.TryRemove(Channel.Id, out List<string> Useless);
+                    PostTrack.TryAdd(Channel.Id, PostIds);
+                }
+            }, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(15));
+            ChannelTimers.TryAdd(Channel.Id, ChannelTimer);
+            return Task.CompletedTask;
         }
 
         public async Task<RedditModel> SubredditAsync(string SubredditName)
@@ -81,8 +71,9 @@ namespace Valerie.Services
             var GetTimer = ChannelTimers[ChannelId];
             GetTimer.Change(Timeout.Infinite, Timeout.Infinite);
             GetTimer.Dispose();
-            GetTimer = null;
-            ChannelTimers.Remove(ChannelId);
+            ChannelTimers.TryRemove(ChannelId, out Timer Useless);
+            Useless.Change(Timeout.Infinite, Timeout.Infinite);
+            Useless.Dispose();
         }
     }
 }
