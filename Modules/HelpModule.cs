4da0d1a70180d8e1327dec4a91ecd81b60ab663d
Yivlx <vuxdev@gmail.com>
2018-05-05 20:59:58 -0700

    Fixed Group Name

--- Modules/HelpModule.cs
+++ Modules/HelpModule.cs
@@ -24,9 +24,9 @@ namespace Valerie.Modules
         {
             var Embed = GetEmbed(Paint.Aqua)
                 .WithAuthor("List of all commands", Context.Client.CurrentUser.GetAvatarUrl())
-                .WithFooter($"For More Information On A Command's Usage: {Context.Config.Prefix}Info CommandName", Emotes.DSupporter.Url);
+                .WithFooter($"For More Information On A Command's Usage: {Context.Config.Prefix}Info CommandName", Emotes.Squint.Url);
             foreach (var Commands in CommandService.Commands.GroupBy(x => x.Module.Name).OrderBy(y => y.Key))
-                Embed.AddField(Commands.Key, string.Join(", ", Commands.Select(x => x.Name).Distinct()));
+                Embed.AddField(Commands.Key, string.Join(", ", Commands.Select(x => $"{x.Module.Group ?? null} {(x.Name.Contains("Async") ? null : x.Name)}").Distinct()));
             return ReplyAsync(string.Empty, Embed.Build());
         }
 
@@ -36,12 +36,13 @@ namespace Valerie.Modules
         {
             var Command = CommandService.Search(Context, CommandName).Commands?.FirstOrDefault().Command;
             if (Command == null) return ReplyAsync($"{CommandName} command doesn't exist.");
+            string Name = Command.Name.Contains("Async") ? Command.Module.Group : Command.Name;
             var Embed = GetEmbed(Paint.Magenta)
                 .WithAuthor("Detailed Command Information", Context.Client.CurrentUser.GetAvatarUrl())
-                .AddField("Name", Command.Name, true)
+                .AddField("Name", Name, true)
                 .AddField("Aliases", string.Join(", ", Command.Aliases), true)
                 .AddField("Arguments", Command.Parameters.Any() ? string.Join(", ", Command.Parameters.Select(x => $"`{x.Type.Name}` {x.Name}")) : "No arguments.")
-                .AddField("Usage", $"{Context.Config.Prefix}{Command.Name} {string.Join(" ", Command.Parameters)}")
+                .AddField("Usage", $"{Context.Config.Prefix} {Name} {string.Join(" ", Command.Parameters)}")
                 .AddField("Summary", Command.Summary)
                 .WithThumbnailUrl(Context.Client.CurrentUser.GetAvatarUrl());
             var GetChar = Command.Parameters.Where(x => x.Type == typeof(char));
