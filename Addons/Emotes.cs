3d3ce5cbe7c87ee28d3f562407cbcddbb24f3e2d
Yivlx <vuxdev@gmail.com>
2018-05-05 20:28:56 -0700

    Added More Emotes

--- Addons/Emotes.cs
+++ Addons/Emotes.cs
@@ -4,13 +4,14 @@ namespace Valerie.Addons
 {
     public class Emotes
     {
-        public static Emote DHeart { get => Emote.Parse("<:hrt:419635124339212308>"); }
-        public static Emote TickNo { get => Emote.Parse("<:tickNo:315009174163685377>"); }
-        public static Emote DWink { get => Emote.Parse("<:dblWink:376811927994761216>"); }
-        public static Emote TickYes { get => Emote.Parse("<:tickYes:315009125694177281>"); }
-        public static Emote DEyes { get => Emote.Parse("<:zoomeyes:390046883281633290>"); }
-        public static Emote PepeSad { get => Emote.Parse("<:PepeSad:420351909833211904>"); }
-        public static Emote DHammer { get => Emote.Parse("<:dblMod:401724520806875139>"); }
-        public static Emote DSupporter { get => Emote.Parse("<:dblSupporter:401725034453925889>"); }
+        public static Emote Bug { get => Emote.Parse("<:Bug:442486343398850560>"); }
+        public static Emote Back { get => Emote.Parse("<:Left:442383594820861992>"); }
+        public static Emote Next { get => Emote.Parse("<:Right:442383594699358208> "); }
+        public static Emote Cross { get => Emote.Parse("<:Cross:442384456083308554>"); }
+        public static Emote Shout { get => Emote.Parse("<:Shout:442495861226733588>"); }
+        public static Emote Squint { get => Emote.Parse("<:Squint:442486320481304586>"); }
+        public static Emote Hammer { get => Emote.Parse("<:Hammer:442490952838545408>"); }
+        public static Emote ThumbUp { get => Emote.Parse("<:ThumbUp:442490952720842782>"); }
+        public static Emote ThumbDown { get => Emote.Parse("<:ThumbDown:442490952310063105>"); }
     }
 }
